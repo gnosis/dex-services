538c85d478951bf1601f7119450ee483236497a9
Ben Smith <bh2smith@gmail.com>
2019-01-22 16:03:47 +0100

    log active event listener

--- event_listener/dfusion_db/management/commands/run_listener.py
+++ event_listener/dfusion_db/management/commands/run_listener.py
@@ -2,10 +2,15 @@ from django.core.management.base import BaseCommand
 from django_eth_events.event_listener import EventListener
 import time
 
+import logging
+
+_log = logging.getLogger(__name__)
+
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
+        _log.info("Event Listener now active")
         event_listener = EventListener()
         while 1:
             event_listener.execute()
