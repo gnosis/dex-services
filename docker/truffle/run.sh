724ebe41f65300ba527c6086a5313e12f096878b
josojo <josojo@hotmail.de>
2019-01-31 23:05:16 +0100

    adding docker file

---
+++ docker/truffle/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd dex-contracts
+
+rm -fr .git || :
+# Compile necesary contracts for app and cleanup unnecesary files
+npm install
+
+# Compile necesary contracts for app and cleanup unnecesary files
+truffle compile
+npx truffle migrate --reset --network developmentdocker
