4c509d07bf2c427bf4e2ef04d288743c257f07be
josojo <josojo@hotmail.de>
2019-02-13 16:06:12 +0100

    changing to invokeViewFunction

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -5,5 +5,5 @@ npx truffle exec scripts/setup_environment.js --network developmentdocker
 npx truffle exec --network developmentdocker  scripts/deposit.js 3 3 18 
 npx truffle exec --network developmentdocker scripts/mineBlocks.js 21 
 sleep 1s
-npx truffle exec --network developmentdocker scripts/viewSnappBaseForCurrentIndex.js 'stateRoots' | grep "0x"> result.txt
+npx truffle exec --network developmentdocker scripts/invokeViewFunction.js 'getCurrentStateRoot' | grep "0x"> result.txt
 diff result.txt ./../test/result_e2e_1.txt
