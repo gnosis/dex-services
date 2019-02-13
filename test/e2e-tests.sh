906c8a5d80d8524ab4680a60c26e668b5db82151
josojo <josojo@hotmail.de>
2019-02-13 16:38:20 +0100

    updating to renaming of scripts in dex-contracts

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -3,7 +3,7 @@ echo $PWD
 
 npx truffle exec scripts/setup_environment.js --network developmentdocker
 npx truffle exec --network developmentdocker  scripts/deposit.js 3 3 18 
-npx truffle exec --network developmentdocker scripts/mineBlocks.js 21 
+npx truffle exec --network developmentdocker scripts/mine_blocks.js 21 
 sleep 1s
 npx truffle exec --network developmentdocker scripts/invokeViewFunction.js 'getCurrentStateRoot' | grep "0x"> result.txt
 diff result.txt ./../test/result_e2e_1.txt
