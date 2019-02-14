62ed0abd1baa69259aa72b2f3ad7b04ae7614f1e
Felix Leupold <felixleupold90@gmail.com>
2019-02-14 17:51:51 +0100

    no grep

--- test/e2e-tests-deposit.sh
+++ test/e2e-tests-deposit.sh
@@ -7,4 +7,4 @@ truffle exec scripts/setup_environment.js
 truffle exec scripts/deposit.js 3 3 18 
 truffle exec scripts/mine_blocks.js 21 
 sleep 1s
-truffle exec scripts/invokeViewFunction.js 'getCurrentStateRoot' | grep 0x77b01abfbad57cb7a1344b12709603ea3b9ad803ef5ea09814ca212748f54733
+truffle exec scripts/invokeViewFunction.js 'getCurrentStateRoot'
