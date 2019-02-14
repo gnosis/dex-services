145c22d5bf528b07a24d91c5ae89efef5d676991
Felix Leupold <felixleupold90@gmail.com>
2019-02-14 16:40:26 +0100

    not using dockercompose network

--- test/e2e-tests-deposit.sh
+++ test/e2e-tests-deposit.sh
@@ -2,8 +2,8 @@
 cd dex-contracts/
 
 # checks state after first new deposit round with a deposit of 18 from account 3 and token 3
-truffle exec scripts/setup_environment.js --network developmentdocker
-truffle exec --network developmentdocker scripts/deposit.js 3 3 18 
-truffle exec --network developmentdocker scripts/mine_blocks.js 21 
+truffle exec scripts/setup_environment.js
+truffle exec scripts/deposit.js 3 3 18 
+truffle exec scripts/mine_blocks.js 21 
 sleep 1s
-truffle exec --network developmentdocker scripts/invokeViewFunction.js 'getCurrentStateRoot' | grep 0x77b01abfbad57cb7a1344b12709603ea3b9ad803ef5ea09814ca212748f54733
+truffle exec scripts/invokeViewFunction.js 'getCurrentStateRoot' | grep 0x77b01abfbad57cb7a1344b12709603ea3b9ad803ef5ea09814ca212748f54733
