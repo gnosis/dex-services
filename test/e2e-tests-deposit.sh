c11030296aac0603171bffd597e94085b295c0a4
Felix Leupold <felixleupold90@gmail.com>
2019-02-14 15:38:44 +0100

    no npx

--- test/e2e-tests-deposit.sh
+++ test/e2e-tests-deposit.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bats
+cd ../dex-contracts/
 
 # checks state after first new deposit round with a deposit of 18 from account 3 and token 3
-npx truffle exec scripts/setup_environment.js --network developmentdocker
-npx truffle exec --network developmentdocker  scripts/deposit.js 3 3 18 
-npx truffle exec --network developmentdocker scripts/mine_blocks.js 21 
+truffle exec scripts/setup_environment.js --network developmentdocker
+truffle exec --network developmentdocker  scripts/deposit.js 3 3 18 
+truffle exec --network developmentdocker scripts/mine_blocks.js 21 
 sleep 1s
-npx truffle exec --network developmentdocker scripts/invokeViewFunction.js 'getCurrentStateRoot' | grep "0x" > result.txt
-npx truffle exec --network developmentdocker scripts/invokeViewFunction.js 'getCurrentStateRoot' | grep 0x77b01abfbad57cb7a1344b12709603ea3b9ad803ef5ea09814ca212748f54733
+truffle exec --network developmentdocker scripts/invokeViewFunction.js 'getCurrentStateRoot' | grep 0x77b01abfbad57cb7a1344b12709603ea3b9ad803ef5ea09814ca212748f54733
