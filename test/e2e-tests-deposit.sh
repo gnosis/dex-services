cf9544b52a01b43af6f852a4c077820eceaf9432
Felix Leupold <felixleupold90@gmail.com>
2019-02-15 15:53:52 +0100

    migrate in e2e

--- test/e2e-tests-deposit.sh
+++ test/e2e-tests-deposit.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bats
 cd dex-contracts/
+truffle migrate
 
 # checks state after first new deposit round with a deposit of 18 from account 3 and token 3
 truffle exec scripts/setup_environment.js
