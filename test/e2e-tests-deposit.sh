7518b360f39b5f0b11ada83cbf8af77d86a5628a
Felix Leupold <felixleupold90@gmail.com>
2019-02-18 15:27:07 +0100

    set -e has to come later

--- test/e2e-tests-deposit.sh
+++ test/e2e-tests-deposit.sh
@@ -1,10 +1,11 @@
-#!/usr/bin/env bats
-set -e
+#!/usr/bin/bash
 
 cd dex-contracts/
 
 # Setup environment, once docker setup is ready (retry up to 5 times)
 for try in {1..5} ; do truffle exec scripts/setup_environment.js && break; done
+
+set -e
 
 # checks state after first new deposit round with a deposit of 18 from account 3 and token 3
 truffle exec scripts/deposit.js 3 3 18 
