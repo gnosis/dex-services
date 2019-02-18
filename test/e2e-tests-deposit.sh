1c7e77e80a692cd628ca0a4fdf5d2124fc1c6a3f
Felix Leupold <felixleupold90@gmail.com>
2019-02-18 18:03:47 +0100

    sleep inside test

--- test/e2e-tests-deposit.sh
+++ test/e2e-tests-deposit.sh
@@ -9,6 +9,8 @@ truffle exec scripts/setup_environment.js
 truffle exec scripts/deposit.js 3 3 18 
 truffle exec scripts/mine_blocks.js 21
 
+sleep 5
+
 EXPECTED_HASH="f1940d119100aae087a3a8f202a23a8b81486908576cb63eb6261fdc72e23b67"
 truffle exec scripts/invokeViewFunction.js 'getCurrentStateRoot' | grep $EXPECTED_HASH
 mongo dfusion2 --eval "db.accounts.findOne({'stateHash': '$EXPECTED_HASH'}).balances[62]" | grep 18
