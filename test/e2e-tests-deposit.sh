07929c133a340587eb394c3f47a1f9959780c75a
Felix Leupold <felixleupold90@gmail.com>
2019-02-18 15:13:56 +0100

    retry

--- test/e2e-tests-deposit.sh
+++ test/e2e-tests-deposit.sh
@@ -2,13 +2,13 @@
 set -e
 
 cd dex-contracts/
-sleep 30s
+
+# Setup environment, once docker setup is ready (retry up to 5 times)
+for try in {1..5} ; do truffle exec scripts/setup_environment.js && break; done
 
 # checks state after first new deposit round with a deposit of 18 from account 3 and token 3
-truffle exec scripts/setup_environment.js
 truffle exec scripts/deposit.js 3 3 18 
-truffle exec scripts/mine_blocks.js 21 
-sleep 5s
+truffle exec scripts/mine_blocks.js 21
 
 EXPECTED_HASH="f1940d119100aae087a3a8f202a23a8b81486908576cb63eb6261fdc72e23b67"
 truffle exec scripts/invokeViewFunction.js 'getCurrentStateRoot' | grep $EXPECTED_HASH
