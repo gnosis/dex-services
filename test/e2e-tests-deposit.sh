97223bb33018a1baef70605d98832df0763bfb8c
Felix Leupold <felixleupold90@gmail.com>
2019-02-18 13:57:35 +0100

    working version

--- test/e2e-tests-deposit.sh
+++ test/e2e-tests-deposit.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bats
+set -e
+
 cd dex-contracts/
 
 # checks state after first new deposit round with a deposit of 18 from account 3 and token 3
 truffle exec scripts/setup_environment.js
 truffle exec scripts/deposit.js 3 3 18 
 truffle exec scripts/mine_blocks.js 21 
-sleep 1s
-truffle exec scripts/invokeViewFunction.js 'getCurrentStateRoot' | grep 0x7b01abfbad57cb7a1344b12709603ea3b9ad803ef5ea09814ca212748f54733
+sleep 5s
+
+EXPECTED_HASH="f1940d119100aae087a3a8f202a23a8b81486908576cb63eb6261fdc72e23b67"
+truffle exec scripts/invokeViewFunction.js 'getCurrentStateRoot' | grep $EXPECTED_HASH
+mongo dfusion2 --eval "db.accounts.findOne({'stateHash': '$EXPECTED_HASH'}).balances[62]" | grep 18
