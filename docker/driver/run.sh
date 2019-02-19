75b33b9e4015a73db17bb01efc4cebf973be44ce
josojo <josojo@hotmail.de>
2019-02-19 13:52:28 +0100

    Setting up the truffle docker + e2e test (#25)
    Will run an end2end test of opening an account, depositing, applying deposit and verifying updated balances under the new root hash in the database.

--- docker/driver/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd driver
-cargo run
---
+++ test/e2e-tests-deposit.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+set -e
+
+cd dex-contracts/
+
+truffle exec scripts/setup_environment.js
+
+# checks state after first new deposit round with a deposit of 18 from account 3 and token 3
+truffle exec scripts/deposit.js 3 3 18 
+truffle exec scripts/mine_blocks.js 21
+
+sleep 5
+
+EXPECTED_HASH="f1940d119100aae087a3a8f202a23a8b81486908576cb63eb6261fdc72e23b67"
+truffle exec scripts/invokeViewFunction.js 'getCurrentStateRoot' | grep $EXPECTED_HASH
+mongo dfusion2 --eval "db.accounts.findOne({'stateHash': '$EXPECTED_HASH'}).balances[62]" | grep 18
