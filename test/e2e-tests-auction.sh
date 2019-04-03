db386668c23d16d3ecbf96e83d40a09965728f98
Ben Smith <bh2smith@gmail.com>
2019-04-03 12:27:44 +0200

    EndToEnd order collection event-listener test

--- test/e2e-tests-auction.sh
+++ test/e2e-tests-auction.sh
@@ -3,6 +3,10 @@ set -e
 
 cd dex-contracts/
 
+EXPECTED_AUCTION=1
+# Ensure no orders yet in auction slot 1
+mongo dfusion2 --eval "db.orders.find({'auctionId': ${EXPECTED_AUCTION}}).size()" | grep 0
+
 # Place 6 orders in current Auction (accountId, buyToken, sellToken, minBuy, maxSell)
 truffle exec scripts/sell_order.js 1 2 3 12 12
 truffle exec scripts/sell_order.js 2 3 2 22 20
@@ -11,19 +15,14 @@ truffle exec scripts/sell_order.js 4 1 2 180 15
 truffle exec scripts/sell_order.js 5 2 1 4 52
 truffle exec scripts/sell_order.js 6 3 1 20 280
 
-truffle exec scripts/mine_blocks.js 21
 sleep 5
 
-# Test Listener (order put into Database) - Query mongodb
-# mongo --eval "db.dfusion.orders.find({'slot': 1})"
-# TODO
+# Test Listener: There are now 6 orders in auction slot 1 and sellAmount for accountId = 6 is 280
+mongo dfusion2 --eval "db.orders.find({'auctionId': ${EXPECTED_AUCTION}}).size()" | grep 6
+mongo dfusion2 --eval "db.orders.findOne({'auctionId': ${EXPECTED_AUCTION}, 'accountId': 6}).sellAmount" | grep 280
 
+truffle exec scripts/mine_blocks.js 21
 
-# Wait for state update because of closed auction
+# TODO - Test Driver: Wait for state update because of closed auction
 
-# TODO
-
-# Check that Auction Settlement is something different
-# In this case the simple solver would match the two orders with agreeing limit prices between two tokens.
-
-# TODO
+# TODO - Test Solver: Ensure that balances are differ after state transition (i.e. found non-trivial solution)
