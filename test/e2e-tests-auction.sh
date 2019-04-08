1df35cbf1e4bfe11a0980e0890493f1b7ddfe066
Benjamin Smith <bh2smith@users.noreply.github.com>
2019-04-08 16:42:10 +0200

    [Bug] Typo in e2e auction data (#84)
    * Update contract code (to include sell-order script)
    include 6 orders from workshop as example
    
    * change accountID on orders placed.
    
    * EndToEnd order collection event-listener test
    
    * bash variable reference fix
    
    * include auction e2e test in travis build
    
    * update contracts
    
    * Typo in orders (was no solution)
    
    * empty commit to restaert travis.
    
    * Rearrange order in which test scripts are run. (i.e. swap auction and withdraw execution order).

--- test/e2e-tests-auction.sh
+++ test/e2e-tests-auction.sh
@@ -4,13 +4,14 @@ set -e
 cd dex-contracts/
 
 EXPECTED_AUCTION=1
+
 # Ensure no orders yet in auction slot 1
 mongo dfusion2 --eval "db.orders.find({'auctionId': ${EXPECTED_AUCTION}}).size()" | grep 0
 
 # Place 6 orders in current Auction (accountId, buyToken, sellToken, minBuy, maxSell)
 truffle exec scripts/sell_order.js 1 2 3 12 12
 truffle exec scripts/sell_order.js 2 3 2 22 20
-truffle exec scripts/sell_order.js 3 1 3 15 10
+truffle exec scripts/sell_order.js 3 1 3 150 10
 truffle exec scripts/sell_order.js 4 1 2 180 15
 truffle exec scripts/sell_order.js 5 2 1 4 52
 truffle exec scripts/sell_order.js 6 3 1 20 280
