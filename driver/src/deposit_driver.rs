b626bb4cab9614f5fc5fef229f7f0b1a4bf1cbeb
josojo <josojo@hotmail.de>
2019-04-12 17:15:44 +0200

    refactoring with hopefully the same beauty as Felix's code

--- driver/src/deposit_driver.rs
+++ driver/src/deposit_driver.rs
@@ -4,8 +4,10 @@ use crate::models::RollingHashable;
 use crate::db_interface::DbInterface;
 use crate::error::{DriverError, ErrorKind};
 use crate::contract::SnappContract;
+use crate::util;
 
-use web3::types::{H256, U256};
+use web3::types::H256;
+
 
 pub fn apply_deposits(
     state: &mut models::State,
@@ -21,74 +23,41 @@ pub fn run_deposit_listener<D, C>(db: &D, contract: &C) -> Result<(bool), Driver
     where   D: DbInterface,
             C: SnappContract
 {
-    let curr_state_root: H256 = contract.get_current_state_root()?;
-    let mut state = db.get_current_balances(&curr_state_root)?;
+    let deposit_slot = contract.get_current_deposit_slot()?;
 
-    // check that operator has sufficient ether
-    
-    let current_deposit_ind: U256 = contract.get_current_deposit_slot()?;
+    println!("Current top deposit_slot is {:?}", deposit_slot);
+    let slot = util::find_first_unapplied_slot(
+        deposit_slot + 1, 
+        Box::new(&|i| contract.has_deposit_slot_been_applied(i))
+    )?;
+    if slot <= deposit_slot {
+        println!("Highest unprocessed deposit_slot is {:?}", slot);
+        if util::can_process(slot, contract,
+            Box::new(&|i| contract.creation_block_for_deposit_slot(i))
+        )? {
+            println!("Processing deposit_slot {:?}", slot);
+            let state_root = contract.get_current_state_root()?;
+            let contract_deposit_hash = contract.deposit_hash_for_slot(slot)?;
+            let mut balances = db.get_current_balances(&state_root)?;
 
-    // get latest non-applied deposit_index
-    let mut deposit_ind = current_deposit_ind + 1;
-    println!("Current top deposit_slot is {:?}", deposit_ind);
-    let mut found: bool = false;
+            let deposits = db.get_deposits_of_slot(slot.low_u32())?;
+            let deposit_hash = deposits.rolling_hash();
+            if deposit_hash != contract_deposit_hash {
+                return Err(DriverError::new(
+                    &format!("Pending deposit hash from contract ({}), didn't match the one found in db ({})", 
+                    deposit_hash, contract_deposit_hash), ErrorKind::StateError
+                ));
+            }
 
-    // Starting from the last depositSlot, we search backwards to the first non-applied deposit
-    while !found && deposit_ind != U256::zero() {
-        deposit_ind = deposit_ind - 1;
-        found = contract.has_deposit_slot_been_applied(deposit_ind)?;
-    }
-    if found {
-        deposit_ind = deposit_ind + 1;
-    }
-    println!("Current pending deposit_slot is {:?}", deposit_ind);
-
-    let current_deposit_ind_block = contract.creation_block_for_deposit_slot(deposit_ind)?;
-    let current_block = contract.get_current_block_number()?;
-
-    let deposit_hash_pulled: H256 = contract.deposit_hash_for_slot(deposit_ind)?;
-    let deposit_slot_empty = deposit_hash_pulled == H256::zero();
-
-    println!(
-        "Current block is {:?} and the last deposit_ind_creationBlock is {:?}",
-        current_block, current_deposit_ind_block
-    );
-
-    // if 20 blocks have past since the first deposit and we are not in the newest slot, we apply the deposit.
-    if current_deposit_ind_block + 20 < current_block
-        && deposit_ind != current_deposit_ind + 1
-    {
-        println!("Next deposit_slot to be processed is {}", deposit_ind);
-        let deposits = db.get_deposits_of_slot(deposit_ind.low_u32())?;
-        println!("Amount of deposits to be processed{:?}", deposits.len());
-        //rehash deposits
-        let deposit_hash: H256 = deposits.rolling_hash();
-
-        if deposit_hash != deposit_hash_pulled {
-            return Err(DriverError::new(
-                &format!("Pending deposit hash from contract ({}), didn't match the one found in db ({})", 
-                deposit_hash, deposit_hash_pulled), ErrorKind::StateError
-            ));
-        }
-
-        if deposit_slot_empty && deposit_ind != U256::zero() {
-            println!("deposit_slot {} already processed", deposit_ind);
-        } else {
-            // calculate new state by applying all deposits
-            state = apply_deposits(&mut state, &deposits);
-            println!("New State_hash is{:?}", state.rolling_hash());
-
-            //send new state into blockchain
-            //applyDeposits signature is (slot, _currStateRoot, _newStateRoot, deposit_slotHash)
-            let slot = U256::from(deposit_ind);
-            let _curr_state_root = curr_state_root;
-            let _new_state_root = H256::from(state.rolling_hash());
-
-            contract.apply_deposits(slot, _curr_state_root, _new_state_root, deposit_hash_pulled)?;
+            let updated_balances = apply_deposits(&mut balances, &deposits);
+            let new_state_root = H256::from(updated_balances.rolling_hash());
+            
+            println!("New State_hash is {}", new_state_root);
+            contract.apply_deposits(slot, state_root, new_state_root, contract_deposit_hash)?;
             return Ok(true);
+        } else {
+            println!("Need to wait before processing deposit_slot {:?}", slot);
         }
-    } else {
-        println!("All deposits are already processed");
     }
     Ok(false)
 }
