96afe59155ea085c5aacc83745f8cc207e1c3144
Felix Leupold <felixleupold90@gmail.com>
2019-05-10 16:43:21 +0200

    [E2E] replacing sleep with retry statement (#117)
    Trying to reduce test time and make them less flaky by using retry instead of sleep

--- test/e2e-tests-auction.sh
+++ test/e2e-tests-auction.sh
@@ -6,7 +6,7 @@ cd dex-contracts/
 EXPECTED_AUCTION=0
 
 # Ensure no orders yet in auction slot 1
-mongo dfusion2 --eval "db.orders.find({'auctionId': ${EXPECTED_AUCTION}}).size()" | grep 0
+mongo dfusion2 --eval "db.orders.find({'auctionId': ${EXPECTED_AUCTION}}).size()" | grep -w 0
 
 # Make sure we have enough balances for the trades
 truffle exec scripts/deposit.js 0 2 300
@@ -24,22 +24,18 @@ truffle exec scripts/sell_order.js 3 0 1 180 15 # executed sell: 4
 truffle exec scripts/sell_order.js 4 1 0 4 52 # executed sell: 52
 truffle exec scripts/sell_order.js 5 2 0 20 280
 
-sleep 5
-
 # Test Listener: There are now 6 orders in auction slot 1 and sellAmount for accountId = 5 is 280000000000000000000
-mongo dfusion2 --eval "db.orders.find({'auctionId': ${EXPECTED_AUCTION}}).size()" | grep 6
-mongo dfusion2 --eval "db.orders.findOne({'auctionId': ${EXPECTED_AUCTION}, 'accountId': 5}).sellAmount" | grep 280000000000000000000
+retry -t 5 "mongo dfusion2 --eval \"db.orders.find({'auctionId': ${EXPECTED_AUCTION}}).size()\" | grep -w 6"
+retry -t 5 "mongo dfusion2 --eval \"db.orders.findOne({'auctionId': ${EXPECTED_AUCTION}, 'accountId': 5}).sellAmount\" | grep -w 280000000000000000000"
 
 truffle exec scripts/wait_seconds.js 181
 
-sleep 10
-
 # Test balances have been updated
 EXPECTED_HASH="0e0369b8be154350fd09141a43d90a53427221001ba9fee5c97145e777420944"
-truffle exec scripts/invokeViewFunction.js 'getCurrentStateRoot' | grep ${EXPECTED_HASH}
+retry -t 5 "truffle exec scripts/invokeViewFunction.js 'getCurrentStateRoot' | grep ${EXPECTED_HASH}"
 
 # Account 4 has now 4 of token 1 
-mongo dfusion2 --eval "db.accounts.findOne({'stateHash': '$EXPECTED_HASH'}).balances[121]" | grep 4000000000000000000
+retry -t 5 "mongo dfusion2 --eval \"db.accounts.findOne({'stateHash': '$EXPECTED_HASH'}).balances[121]\" | grep -w 4000000000000000000"
 
 # Account 3 has now 52 of token 0
-mongo dfusion2 --eval "db.accounts.findOne({'stateHash': '$EXPECTED_HASH'}).balances[90]" | grep 52000000000000000000
+retry -t 5 "mongo dfusion2 --eval \"db.accounts.findOne({'stateHash': '$EXPECTED_HASH'}).balances[90]\" | grep -2 52000000000000000000"
