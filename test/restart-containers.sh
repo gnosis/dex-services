6d30979df5418f31df0c10cc8ad515d64f9a8230
josojo <josojo@hotmail.de>
2019-07-03 15:45:47 +0200

    use rm -sf container, instead of kill and rm

--- test/restart-containers.sh
+++ test/restart-containers.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -e
 
-docker-compose kill mongo
-docker-compose rm -f mongo
+docker-compose rm -sf mongo
 docker-compose up -d mongo
-docker-compose kill ganache-cli 
-docker-compose rm -f ganache-cli
+docker-compose rm -sf ganache-cli
 docker-compose up -d ganache-cli
 docker-compose restart listener
 cd dex-contracts && retry truffle migrate && cd -
