3a48a730caf0c17d11ea39cc5d8f553f2f2c64e8
Benjamin Smith <bh2smith@users.noreply.github.com>
2019-07-03 16:25:31 +0200

    Independent End to End Testing (#162)
    * update contracts - to include ganache snapshot scripts
    
    * bash scripts to take ganach snapshots and setup contract environment (account/token registration and account funding)
    
    * update e2e-tests using ganach snapshots and mongo-db reset
    
    * test e2e example from standing order
    
    * clean-up for making test independent with docker-container restart
    
    * revert include e2e-test as not directly related to task
    
    * adding missing restart-container script
    
    * restarting docker containers externally, not in e2e test iteself
    
    * removing restart container before first test
    
    * use rm -sf container, instead of kill and rm
    
    * must restart container before the first test - since restart includes truffle migrate.

---
+++ test/restart-containers.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+docker-compose rm -sf mongo
+docker-compose up -d mongo
+docker-compose rm -sf ganache-cli
+docker-compose up -d ganache-cli
+docker-compose restart listener
+cd dex-contracts && retry truffle migrate && cd -
