0602044c112cc64d9163efb9e0ad35cb11145628
Felix Leupold <felixleupold90@gmail.com>
2019-07-24 17:09:28 +0200

    [easy] refactor util functions into traits

--- dfusion_rust_core/src/models/util.rs
+++ dfusion_rust_core/src/models/util.rs
@@ -5,34 +5,85 @@ use std::convert::TryFrom;
 use std::str::FromStr;
 use web3::types::{U256, H256};
 
-pub fn pop_u8_from_log_data(bytes: &mut Vec<u8>) -> u8 {
-    pop_u256_from_log_data(bytes).as_u32().try_into().unwrap()
+pub trait PopFromLogData {
+    fn pop_from_log_data(bytes: &mut Vec<u8>) -> Self;
 }
 
-pub fn pop_u16_from_log_data(bytes: &mut Vec<u8>) -> u16 {
-    pop_u256_from_log_data(bytes).as_u32().try_into().unwrap()
+impl PopFromLogData for u8 {
+    fn pop_from_log_data(bytes: &mut Vec<u8>) -> Self {
+        U256::pop_from_log_data(bytes).as_u32().try_into().unwrap()
+    }
 }
 
-pub fn pop_u128_from_log_data(bytes: &mut Vec<u8>) -> u128 {
-    pop_u256_from_log_data(bytes).to_string().parse().unwrap()
+impl PopFromLogData for u16 {
+    fn pop_from_log_data(bytes: &mut Vec<u8>) -> Self {
+        U256::pop_from_log_data(bytes).as_u32().try_into().unwrap()
+    }
 }
 
-pub fn pop_u256_from_log_data(bytes: &mut Vec<u8>) -> U256 {
-    U256::from_big_endian(
-        bytes.drain(0..32).collect::<Vec<u8>>().as_slice()
-    )
+impl PopFromLogData for u128 {
+    fn pop_from_log_data(bytes: &mut Vec<u8>) -> Self {
+        U256::pop_from_log_data(bytes).to_string().parse().unwrap()
+    }
 }
 
-pub fn pop_h256_from_log_data(bytes: &mut Vec<u8>) -> H256 {
-    H256::from(
-        bytes.drain(0..32).collect::<Vec<u8>>().as_slice()
-    )
+impl PopFromLogData for U256 {
+    fn pop_from_log_data(bytes: &mut Vec<u8>) -> Self {
+        U256::from_big_endian(
+            bytes.drain(0..32).collect::<Vec<u8>>().as_slice()
+        )
+    }
 }
 
-pub fn to_value<T: ToString>(value: &T) -> Value {
-    Value::from(
-        BigDecimal::from_str(&value.to_string()).unwrap()
-    )
+impl PopFromLogData for H256 {
+    fn pop_from_log_data(bytes: &mut Vec<u8>) -> Self {
+        H256::from(
+            bytes.drain(0..32).collect::<Vec<u8>>().as_slice()
+        )
+    }
+}
+
+pub trait ToValue {
+    fn to_value(&self) -> Value;
+}
+
+impl ToValue for u8 {
+    fn to_value(&self) -> Value {
+        i32::from(*self).into()
+    }
+}
+
+impl ToValue for u16 {
+    fn to_value(&self) -> Value {
+        i32::from(*self).into()
+    }
+}
+
+impl ToValue for u128 {
+    fn to_value(&self) -> Value {
+        BigDecimal::from_str(&self.to_string()).unwrap().into()
+    }
+}
+
+impl ToValue for U256 {
+    fn to_value(&self) -> Value {
+        BigDecimal::from_str(&self.to_string()).unwrap().into()
+    }
+}
+
+impl ToValue for H256 {
+    fn to_value(&self) -> Value {
+        format!("{:x}", self).into()
+    }
+}
+
+impl ToValue for Vec<u128> {
+    fn to_value(&self) -> Value {
+        self.iter()
+            .map(|balance| balance.to_value())
+            .collect::<Vec<Value>>()
+            .into()
+    }
 }
 
 pub trait EntityParsing {
