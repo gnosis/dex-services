d6712faac3356ac9a7de0f34063bf7135e3e4123
Anxo Rodriguez <anxolin@gmail.com>
2019-08-05 13:05:48 +0200

    Use npx for truffle instead of global system one

--- test/e2e-tests-deposit-withdraw.sh
+++ test/e2e-tests-deposit-withdraw.sh
@@ -4,14 +4,14 @@ cd dex-contracts
 source ../test/utils.sh
 
 step "Setup" \
-"truffle exec scripts/setup_environment.js 6"
+"npx truffle exec scripts/setup_environment.js 6"
 
 ###############
 # Deposit Tests
 ###############
 
 step "Deposit 18 of token 2 for user 2" \
-"truffle exec scripts/deposit.js 2 2 18"
+"npx truffle exec scripts/deposit.js 2 2 18"
 
 step_with_retry "Deposit was added to graph db" \
 "source ../test/utils.sh && query_graphql \
@@ -22,12 +22,12 @@ step_with_retry "Deposit was added to graph db" \
     }\" | grep 18000000000000000000"
 
 step "Advance time to finalize batch" \
-"truffle exec scripts/wait_seconds.js 181"
+"npx truffle exec scripts/wait_seconds.js 181"
 
 EXPECTED_HASH="73815c173218e6025f7cb12d0add44354c4671e261a34a360943007ff6ac7af5"
 
 step_with_retry "Check contract updated" \
-"truffle exec scripts/invokeViewFunction.js 'getCurrentStateRoot' | grep ${EXPECTED_HASH}"
+"npx truffle exec scripts/invokeViewFunction.js 'getCurrentStateRoot' | grep ${EXPECTED_HASH}"
 
 step_with_retry "Check mongo db updated" \
 "mongo dfusion2 --eval \"db.accounts.findOne({'stateHash': '${EXPECTED_HASH}'}).balances[62]\" | grep -w 18000000000000000000"
@@ -45,7 +45,7 @@ step_with_retry "Check graph db updated" \
 ################
 
 step "Request withdraw of 18 of token 2 by account 2" \
-    "truffle exec scripts/request_withdraw.js 2 2 18"
+    "npx truffle exec scripts/request_withdraw.js 2 2 18"
 
 step_with_retry "Withdraw was added to graph db" \
 "source ../test/utils.sh && query_graphql \
@@ -55,17 +55,17 @@ step_with_retry "Withdraw was added to graph db" \
         } \
     }\" | grep 18000000000000000000"
 
-step "wait till withdraw slot becomes inactive" "truffle exec scripts/wait_seconds.js 181"
+step "wait till withdraw slot becomes inactive" "npx truffle exec scripts/wait_seconds.js 181"
 
 EXPECTED_HASH="7b738197bfe79b6d394499b0cac0186cdc2f65ae2239f2e9e3c698709c80cb67"
 
 step_with_retry "Check contract updated" \
-"truffle exec scripts/invokeViewFunction.js 'getCurrentStateRoot' | grep ${EXPECTED_HASH}"
+"npx truffle exec scripts/invokeViewFunction.js 'getCurrentStateRoot' | grep ${EXPECTED_HASH}"
 
 step_with_retry "Check mongo db updated" \
  "mongo dfusion2 --eval \"db.accounts.findOne({'stateHash': '$EXPECTED_HASH'}).balances[62]\" | grep -w 0"
 
 # Should now be able to claim withdraw and see a balance change
 step "Claim Withdraw" \
-"truffle exec scripts/claim_withdraw.js 0 2 2 | grep \"Success! Balance of token 2 before claim: 282000000000000000000, after claim: 300000000000000000000\""
+"npx truffle exec scripts/claim_withdraw.js 0 2 2 | grep \"Success! Balance of token 2 before claim: 282000000000000000000, after claim: 300000000000000000000\""
 
