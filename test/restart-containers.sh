2706d75ee58ea606fc1a5dc69c9b6a23370902ba
Anxo Rodriguez <anxolin@gmail.com>
2019-08-05 19:51:42 +0200

    Restart all services in restart-containers.sh

--- test/restart-containers.sh
+++ test/restart-containers.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-docker-compose rm -sf mongo
-docker-compose up -d mongo
-docker-compose rm -sf ganache-cli
-docker-compose up -d ganache-cli
-docker-compose restart listener
+docker-compose rm -sf
+docker-compose up -d
 cd dex-contracts && wait-port -t 30000 8545 && truffle migrate && cd -
