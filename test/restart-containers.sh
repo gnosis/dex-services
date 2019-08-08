3127893ec45efa3469ec1ceaa3a63de929d42ea1
Anxo Rodriguez <anxolin@gmail.com>
2019-08-08 16:23:11 +0200

    Restart all services in restart-containers.sh (#210)
    * Restart all services in restart-containers.sh
    
    * restart postgres, graph-listener, use npx

--- test/restart-containers.sh
+++ test/restart-containers.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 set -e
 
+
 docker-compose rm -sf mongo
 docker-compose up -d mongo
+
+docker-compose rm -sf postgres
+docker-compose up -d postgres
+
 docker-compose rm -sf ganache-cli
 docker-compose up -d ganache-cli
+
 docker-compose restart listener
-cd dex-contracts && wait-port -t 30000 8545 && truffle migrate && cd -
+docker-compose restart graph-listener
+
+cd dex-contracts && npx wait-port -t 30000 8545 && npx truffle migrate && cd -
