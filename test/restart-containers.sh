aede0245a49b26a36a2d51932a7af33502e1062c
David Sato <david.sato64@gmail.com>
2019-08-08 16:03:13 +0200

    restart postgres, graph-listener, use npx

--- test/restart-containers.sh
+++ test/restart-containers.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 set -e
 
-docker-compose rm -sf
-docker-compose up -d
-cd dex-contracts && wait-port -t 30000 8545 && truffle migrate && cd -
+
+docker-compose rm -sf mongo
+docker-compose up -d mongo
+
+docker-compose rm -sf postgres
+docker-compose up -d postgres
+
+docker-compose rm -sf ganache-cli
+docker-compose up -d ganache-cli
+
+docker-compose restart listener
+docker-compose restart graph-listener
+
+cd dex-contracts && npx wait-port -t 30000 8545 && npx truffle migrate && cd -
