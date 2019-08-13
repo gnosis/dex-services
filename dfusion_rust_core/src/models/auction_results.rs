98b67c4b5da318f16e10f9fbe42c7ea0c0e2f846
Ben Smith <bh2smith@gmail.com>
2019-08-13 15:42:22 +0200

    Linting (whitespace)

--- dfusion_rust_core/src/models/auction_results.rs
+++ dfusion_rust_core/src/models/auction_results.rs
@@ -2,7 +2,7 @@
 pub struct AuctionResults {
     pub prices: Vec<u128>,
     pub buy_amounts: Vec<u128>,
-    pub sell_amounts: Vec<u128>
+    pub sell_amounts: Vec<u128>,
 }  //TODO - Use Solution from driver/src/price_finding/price_finder_interface.rs
 
 
@@ -11,9 +11,9 @@ impl From<Vec<u8>> for AuctionResults {
         println!("{:?}", solution_data[0]);
         // TODO - parse solution_data!
         AuctionResults {
-            prices: vec![0 as u128;1],
-            buy_amounts: vec![0 as u128;1],
-            sell_amounts: vec![0 as u128;1]
+            prices: vec![0 as u128; 1],
+            buy_amounts: vec![0 as u128; 1],
+            sell_amounts: vec![0 as u128; 1],
         }
     }
 }
