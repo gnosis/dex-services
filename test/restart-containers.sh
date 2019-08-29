c8e38f5808b53437b133875fa575a9e2b12b20e9
Felix Leupold <felixleupold90@gmail.com>
2019-08-29 15:47:35 +0200

    remove postgres since it contains info about previous chain state

--- test/restart-containers.sh
+++ test/restart-containers.sh
@@ -2,6 +2,7 @@
 set -e
 
 docker-compose kill
+docker-compose rm -sf postgres
 docker-compose up -d driver graph-listener
 
 cd dex-contracts && npx wait-port -t 30000 8545 && npx truffle migrate && cd -
