87824c3d867982676db98e13b80fe8b7ebcb611b
Felix Leupold <felixleupold90@gmail.com>
2019-08-30 08:42:26 +0200

    Using docker-compose kill to speed up restarts (#232)
    Restarting the containers between tests takes ~60s at the moment (cf. https://travis-ci.org/gnosis/dex-services/jobs/578335529#L530)
    
    With this change I was able to reduce the time between restarts to <15s. This should lead to a speedup of ~1.5mn for our overall travis time.
    
    ### TestPlan
    
    Observe restart-container.sh timing in travis.

--- test/restart-containers.sh
+++ test/restart-containers.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 set -e
 
+docker-compose kill
 docker-compose rm -sf postgres
-docker-compose up -d postgres
-
-docker-compose rm -sf ganache-cli
-docker-compose up -d ganache-cli
-
-docker-compose restart graph-listener
-docker-compose restart driver
+docker-compose up -d driver graph-listener
 
 cd dex-contracts && npx wait-port -t 30000 8545 && npx truffle migrate && cd -
