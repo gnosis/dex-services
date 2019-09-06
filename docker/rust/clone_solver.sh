422e400fba152c23585a9826a1efd4ea08a4259b
Felix Leupold <felixleupold90@gmail.com>
2019-09-06 14:34:26 +0200

    Fixing linear optimzer (#236)
    Using the linear solver was broken (like everything that doesn't run as part of CI usually is), because the path from which the driver is run changed one up (when moving from rust projects to rust workspace).
    
    I used this opportunity to adjust the repo to the new gitlab repo, add some explanation in the README and pin the repo against a version that we know works with the old prototype.
    
    When running the stablecoin excange, we'll likely make the tag/rev an environment variable so it can be different for StableX and dƒusion.
    
    ### Test Plan
    
    Follow the steps in the readme to make sure the linear optimiser is giving good auction results (I ran the auciton e2e test to try it out).

--- docker/rust/clone_solver.sh
+++ docker/rust/clone_solver.sh
@@ -4,13 +4,14 @@ set -e
 # Authorize SSH Host
 mkdir -p /root/.ssh
 chmod 0700 /root/.ssh && \
-ssh-keyscan gitlab.com > /root/.ssh/known_hosts
+ssh-keyscan gitlab.gnosisdev.com > /root/.ssh/known_hosts
 
 # Copy SSH key
 cp .ssh/id_rsa /root/.ssh/id_rsa
 cp .ssh/id_rsa.pub /root/.ssh/id_rsa.pub
 
 # Clone and install dependencies
-git clone git@gitlab.com:twalth3r/batchauctions.git
+git clone git@gitlab.gnosisdev.com:dfusion/batchauctions.git
 cd batchauctions
+git checkout v0.1
 pip install -r requirements.txt
