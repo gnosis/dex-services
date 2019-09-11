c00f1419336feda0cf87ba98e332a7133c02fbb5
Ben Smith <bh2smith@gmail.com>
2019-09-11 10:36:06 +0200

    Implement both submit_solution and get_current_auction_index (since they are easy). Had to upcast u16 to U128 in order to use .call for submit_solution parameters.

--- driver/src/contracts/stablex_contract.rs
+++ driver/src/contracts/stablex_contract.rs
@@ -3,10 +3,9 @@ extern crate mock_it;
 
 use dfusion_core::models::{AccountState, Order};
 
-// TODO - uncomment when removing "unimplemented!()"
-//use web3::contract::Options;
-//use web3::futures::Future;
-use web3::types::{H160, U128};
+use web3::contract::Options;
+use web3::futures::Future;
+use web3::types::{H160, U128, U256};
 
 use crate::error::DriverError;
 
@@ -36,43 +35,54 @@ impl StableXContractImpl {
 }
 
 pub trait StableXContract {
-    fn get_current_auction_index(&self) -> Result<u32>;
-    fn get_auction_data(&self, _index: u32) -> Result<(AccountState, Vec<Order>)>;
+    fn get_current_auction_index(&self) -> Result<U256>;
+    fn get_auction_data(&self, _index: U256) -> Result<(AccountState, Vec<Order>)>;
 
     fn submit_solution(
         &self,
-        _batch_index: u32,
-        _owners: Vec<H160>,
-        _order_ids: Vec<u16>,
-        _volumes: Vec<U128>,
-        _prices: Vec<U128>,
-        _token_ids_for_price: Vec<u16>,
+        batch_index: U256,
+        owners: Vec<H160>,
+        order_ids: Vec<U128>,
+        volumes: Vec<U128>,
+        prices: Vec<U128>,
+        token_ids_for_price: Vec<U128>,
     ) -> Result<()>;
 }
 
 impl StableXContract for StableXContractImpl {
-    fn get_current_auction_index(&self) -> Result<u32> {
-        unimplemented!();
-//        self.base.contract
-//            .query("getCurrentStateIndex", (), None, Options::default(), None)
-//            .wait()
-//            .map_err(DriverError::from)
+    fn get_current_auction_index(&self) -> Result<U256> {
+        self.base.contract
+            .query("getCurrentStateIndex", (), None, Options::default(), None)
+            .wait()
+            .map_err(DriverError::from)
     }
 
-    fn get_auction_data(&self, _index: u32) -> Result<(AccountState, Vec<Order>)> {
+    fn get_auction_data(&self, _index: U256) -> Result<(AccountState, Vec<Order>)> {
         unimplemented!();
     }
 
     fn submit_solution(
         &self,
-        _batch_index: u32,
-        _owners: Vec<H160>,
-        _order_ids: Vec<u16>,
-        _volumes: Vec<U128>,
-        _prices: Vec<U128>,
-        _token_ids_for_price: Vec<u16>,
+        batch_index: U256,
+        owners: Vec<H160>,
+        order_ids: Vec<U128>,
+        volumes: Vec<U128>,
+        prices: Vec<U128>,
+        token_ids_for_price: Vec<U128>,
     ) -> Result<()> {
-        unimplemented!();
+        let account = self.base
+            .account_with_sufficient_balance()
+            .ok_or("Not enough balance to send Txs")?;
+        self.base.contract
+            .call(
+                "submitSolution",
+               (batch_index, owners, order_ids, volumes, prices, token_ids_for_price),
+               account,
+               Options::default()
+            )
+            .wait()
+            .map_err(DriverError::from)
+            .map(|_| ())
     }
 }
 
@@ -87,18 +97,18 @@ pub mod tests {
     use super::*;
 
     type SubmitSolutionArguments = (
-        u32,
+        U256,
         Matcher<Vec<H160>>,
-        Matcher<Vec<u16>>,
         Matcher<Vec<U128>>,
         Matcher<Vec<U128>>,
-        Matcher<Vec<u16>>,
+        Matcher<Vec<U128>>,
+        Matcher<Vec<U128>>,
     );
 
     #[derive(Clone)]
     pub struct StableXContractMock {
-        pub get_current_auction_index: Mock<(), Result<u32>>,
-        pub get_auction_data: Mock<u32, Result<(AccountState, Vec<Order>)>>,
+        pub get_current_auction_index: Mock<(), Result<U256>>,
+        pub get_auction_data: Mock<U256, Result<(AccountState, Vec<Order>)>>,
         pub submit_solution: Mock<SubmitSolutionArguments, Result<()>>,
     }
 
@@ -122,20 +132,20 @@ pub mod tests {
     }
 
     impl StableXContract for StableXContractMock {
-        fn get_current_auction_index(&self) -> Result<u32> {
+        fn get_current_auction_index(&self) -> Result<U256> {
             self.get_current_auction_index.called(())
         }
-        fn get_auction_data(&self, index: u32) -> Result<(AccountState, Vec<Order>)> {
+        fn get_auction_data(&self, index: U256) -> Result<(AccountState, Vec<Order>)> {
             self.get_auction_data.called(index)
         }
         fn submit_solution(
             &self,
-            batch_index: u32,
+            batch_index: U256,
             owners: Vec<H160>,
-            order_ids: Vec<u16>,
+            order_ids: Vec<U128>,
             volumes: Vec<U128>,
             prices: Vec<U128>,
-            token_ids_for_price: Vec<u16>,
+            token_ids_for_price: Vec<U128>,
         ) -> Result<()> {
             self.submit_solution.called(
                 (
