7b78c74be4426062520078bca2de3e9cc7fd2be8
Felix Leupold <felixleupold90@gmail.com>
2019-09-12 16:06:51 +0200

    Rustfmt as part of travis

---
+++ travis/cargo_format.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+cargo format
+
+if [[ $(git diff --stat) != '' ]]; then
+  echo 'Cargo format caused changes, pushing updated version'
+
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+
+  git commit -am "Travis autoformatting in build: $TRAVIS_BUILD_NUMBER"
+
+  git remote add origin https://${GITHUB_GNOSIS_INFO_API_TOKEN}@github.com/gnosis/dex-services.git > /dev/null 2>&1
+  git push --quiet origin
+else
+  echo 'Cargo format was already clean'
+fi
