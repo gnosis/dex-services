e41b43d67e36cb0737c24e9b829c9e3696ae757c
Felix Leupold <felixleupold90@gmail.com>
2019-09-27 17:50:25 +0200

    [Stablex] Better driver logging (#266)
    Right now our log output is pretty messy and verbose. This PR addresses it in the following way:
    
    1. No longer log every 5 seconds that we have already applied the current batch
    2. Don't invoke the solver on empty auctions (leading to some complaints in the solver)
    3. Don't submit solutions that don't have any volume (and thus also no utility). That would just be waisting gas.
    
    On top of that we update the smart contracts to not show the annoying error message about a Set not being readable before there are any entries.
    
    ### Test Plan
    
    Running the demo with the linear solver, will now show
    
    ```
    stablex_1         |     Finished dev [unoptimized + debuginfo] target(s) in 0.23s
    stablex_1         |      Running `target/debug/stablex`
    stablex_1         | 2019-09-26 16:22:50,937 INFO  [driver::util] Using linear optimisation price finder
    stablex_1         | 2019-09-26 16:22:50,988 INFO  [driver::driver::stablex_driver] No orders in batch 5231715
    stablex_1         | 2019-09-26 16:22:50,988 INFO  [driver::driver::stablex_driver] Not submitting 0 volume solution for batch 5231715
    stablex_1         | 2019-09-26 16:23:54,621 INFO  [driver::driver::stablex_driver] Computed solution: Solution { surplus: Some(3596998402997999639577936039039039039039), prices: [1000000000000000000, 2000000000000000000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], executed_buy_amounts: [998999000999999899899, 1996000003997999800000], executed_sell_amounts: [1999997999999999799597, 998999000999999899899] }
    stablex_1         | 2019-09-26 16:23:54,987 INFO  [driver::driver::stablex_driver] Successfully applied solution to batch 5231716
    stablex_1         | 2019-09-26 16:24:02,981 INFO  [driver::driver::stablex_driver] Computed solution: Solution { surplus: Some(3596998403362023701801864760696680), prices: [1000000000000000000, 2000000000000000000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], executed_buy_amounts: [998999001101101, 1996000004200000], executed_sell_amounts: [1999998000202404, 998999001101101] }
    ```

--- driver/src/driver/stablex_driver.rs
+++ driver/src/driver/stablex_driver.rs
@@ -2,6 +2,8 @@ use crate::contracts::stablex_contract::StableXContract;
 use crate::error::DriverError;
 use crate::price_finding::PriceFinding;
 
+use dfusion_core::models::Solution;
+
 use std::collections::HashSet;
 
 use web3::types::U256;
@@ -25,17 +27,28 @@ impl<'a> StableXDriver<'a> {
         // Try to process previous batch auction
         let batch = self.contract.get_current_auction_index()? - 1;
         if self.past_auctions.contains(&batch) {
-            info!("Already processed batch {}", batch);
             return Ok(false);
         }
         let (account_state, orders) = self.contract.get_auction_data(batch)?;
-        let solution = self.price_finder.find_prices(&orders, &account_state)?;
+        let solution = if orders.is_empty() {
+            info!("No orders in batch {}", batch);
+            Solution::trivial(0)
+        } else {
+            let solution = self.price_finder.find_prices(&orders, &account_state)?;
+            info!("Computed solution: {:?}", &solution);
+            solution
+        };
 
-        info!("Submitting solution: {:?}", &solution);
-        self.contract.submit_solution(batch, orders, solution)?;
+        let submitted = if solution.executed_sell_amounts.iter().sum::<u128>() > 0 {
+            self.contract.submit_solution(batch, orders, solution)?;
+            info!("Successfully applied solution to batch {}", batch);
+            true
+        } else {
+            info!("Not submitting trivial solution for batch {}", batch);
+            false
+        };
         self.past_auctions.insert(batch);
-        info!("Successfully applied solution to batch {}", batch);
-        Ok(true)
+        Ok(submitted)
     }
 }
 
@@ -47,7 +60,6 @@ mod tests {
 
     use dfusion_core::models::account_state::test_util::*;
     use dfusion_core::models::order::test_util::create_order_for_test;
-    use dfusion_core::models::Solution;
 
     use mock_it::Matcher::{Any, Val};
 
@@ -169,5 +181,65 @@ mod tests {
         let mut driver = StableXDriver::new(&contract, &mut pf);
 
         assert!(driver.run().is_err())
+    }
+
+    #[test]
+    fn test_do_not_invoke_solver_when_no_orders() {
+        let contract = StableXContractMock::default();
+        let mut pf = PriceFindingMock::default();
+
+        let orders = vec![];
+        let state = create_account_state_with_balance_for(&orders);
+
+        let batch = U256::from(42);
+        contract
+            .get_current_auction_index
+            .given(())
+            .will_return(Ok(batch));
+
+        contract
+            .get_auction_data
+            .given(batch - 1)
+            .will_return(Ok((state.clone(), orders.clone())));
+
+        let mut driver = StableXDriver::new(&contract, &mut pf);
+
+        assert!(driver.run().is_ok());
+        assert!(!mock_it::verify(
+            pf.find_prices.was_called_with((orders, state))
+        ));
+    }
+
+    #[test]
+    fn test_does_not_submit_empty_solution() {
+        let contract = StableXContractMock::default();
+        let mut pf = PriceFindingMock::default();
+
+        let orders = vec![create_order_for_test(), create_order_for_test()];
+        let state = create_account_state_with_balance_for(&orders);
+
+        let batch = U256::from(42);
+        contract
+            .get_current_auction_index
+            .given(())
+            .will_return(Ok(batch));
+
+        contract
+            .get_auction_data
+            .given(batch - 1)
+            .will_return(Ok((state.clone(), orders.clone())));
+
+        let solution = Solution::trivial(orders.len());
+        pf.find_prices
+            .given((orders, state))
+            .will_return(Ok(solution));
+
+        let mut driver = StableXDriver::new(&contract, &mut pf);
+        assert!(driver.run().is_ok());
+        assert!(!mock_it::verify(
+            contract
+                .submit_solution
+                .was_called_with((batch - 1, Any, Any))
+        ));
     }
 }
