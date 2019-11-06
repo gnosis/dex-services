9f8bea94ae989470d4f1009f4de329892b51dcad
Felix Leupold <felixleupold90@gmail.com>
2019-11-06 15:12:38 +0100

    removing commented code

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -2,21 +2,19 @@
 
 set -euo pipefail
 
-#if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
-  sudo apt-get update && sudo apt-get install -y python-pip && sudo pip install awscli
+sudo apt-get update && sudo apt-get install -y python-pip && sudo pip install awscli
 
-  # Get login token and execute login
-  $(aws ecr get-login --no-include-email --region $AWS_REGION)
-  echo -e $GITLAB_PRIVATE_KEY > .ssh/id_rsa
-  chmod 0500 .ssh/id_rsa
+# Get login token and execute login
+$(aws ecr get-login --no-include-email --region $AWS_REGION)
+echo -e $GITLAB_PRIVATE_KEY > .ssh/id_rsa
+chmod 0500 .ssh/id_rsa
 
-  echo "Building latest image with solver...";
-  docker-compose build --build-arg use_solver=1 stablex
-  docker tag stablex $REGISTRY_URI
+echo "Building latest image with solver...";
+docker-compose build --build-arg use_solver=1 stablex
+docker tag stablex $REGISTRY_URI:$TRAVIS_BRANCH
 
-  echo "Pushing image";
-  docker push $REGISTRY_URI
+echo "Pushing image";
+docker push $REGISTRY_URI:$TRAVIS_BRANCH
 
-  echo "The image has been pushed";
-  rm -rf .ssh/*
-#fi
+echo "The image has been pushed";
+rm -rf .ssh/*
