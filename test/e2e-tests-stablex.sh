10bbbd95d3b14055de04df72787359fbde8bbc96
Benjamin Smith <bh2smith@users.noreply.github.com>
2019-11-12 15:20:09 +0100

    Updates to newest solc version 0.5.5 and set contracts to 4 minute batch PR  (#292)

--- test/e2e-tests-stablex.sh
+++ test/e2e-tests-stablex.sh
@@ -14,7 +14,7 @@ step "Place 2 orders in current auction" \
 "npx truffle exec scripts/stablex/place_order.js --accountId=0 --buyToken=1 --sellToken=0 --minBuy=999 --maxSell=2000 --validFor=20 && \
 npx truffle exec scripts/stablex/place_order.js --accountId=1 --buyToken=0 --sellToken=1 --minBuy=1996 --maxSell=999 --validFor=20 "
 
-step "Advance time to start auction" \
+step "Advance time to close auction" \
 "npx truffle exec scripts/wait_seconds.js 300"
 
 step "Request withdraw" \
