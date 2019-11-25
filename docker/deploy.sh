5518586fa3335b03351afa40bfb54ae22dd57356
Felix Leupold <felixleupold90@gmail.com>
2019-11-25 10:53:30 +0100

    [CI] Add rinkeby e2e test to CI

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -9,9 +9,8 @@ $(aws ecr get-login --no-include-email --region $AWS_REGION)
 echo -e $GITLAB_PRIVATE_KEY > .ssh/id_rsa
 chmod 0500 .ssh/id_rsa
 
-echo "Building latest image with solver...";
-docker-compose build --build-arg use_solver=1 stablex
-docker tag stablex $REGISTRY_URI:$TRAVIS_BRANCH
+echo "Tagging latest image with solver...";
+docker tag stablex-rinkeby $REGISTRY_URI:$TRAVIS_BRANCH
 
 echo "Pushing image";
 docker push $REGISTRY_URI:$TRAVIS_BRANCH
