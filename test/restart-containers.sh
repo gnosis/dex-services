5e6bf5b164ae90d4f19abb8e5e111a92455fc510
Felix Leupold <felixleupold90@gmail.com>
2019-12-03 19:45:30 +0100

    use binary for dfusion e2e tests

--- test/restart-containers.sh
+++ test/restart-containers.sh
@@ -3,6 +3,6 @@ set -e
 
 docker-compose kill
 docker-compose rm -sf postgres
-docker-compose up -d driver graph-listener
+docker-compose up -f docker-compose.yml -f docker-compose.binary.yml -d driver graph-listener
 
 cd dex-contracts && npx wait-port -t 30000 8545 && npx truffle migrate && cd -
