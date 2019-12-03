e040c1fd3b67327a6167b1d60694a8fd70238ec5
Felix Leupold <felixleupold90@gmail.com>
2019-12-03 12:19:33 +0100

    Moving travis file to multi-staged build
    build contract
    
    install awscli
    
    Uploading the correct (binary) image
    
    rename yaml -> yml
    
    Combine unit test and build stage
    
    use binary for dfusion e2e tests
    
    renaming binary
    
    Fix docker-compose command in restart script
    
    sleep so that ganache can start in time
    
    rearrange restart container so that node is already runing when listener starts
    
    Trying to copy build artifacts from host into container
    
    install rust
    
    remove deploy section
    
    interactive install
    
    rustup add no longer need
    
    add cargo path
    
    focus on buidling
    
    xenial
    
    fix cache stage
    
    fixing node build
    
    proper node version
    
    repinstalled rust
    
    Fix node cache and reintroduce unit tests
    
    Change language of node warmup
    
    install cluppy and rustfmt
    
    install ca certificates for https infura access
    
    push solver image
    
    back to using existing images
    
    cleanup
    
    recreating venv
    
    No npm install on build
    
    don't copy contract artifacts
    
    don't copy contract artifacts
    
    typo
    
    comments

--- test/restart-containers.sh
+++ test/restart-containers.sh
@@ -3,6 +3,9 @@ set -e
 
 docker-compose kill
 docker-compose rm -sf postgres
-docker-compose up -d driver graph-listener
 
+docker-compose up -d ganache-cli
 cd dex-contracts && npx wait-port -t 30000 8545 && npx truffle migrate && cd -
+
+docker-compose -f docker-compose.yml -f docker-compose.binary.yml up -d driver graph-listener
+
