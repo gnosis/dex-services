be1f9dc22cf902dc7d00d8131076ecfaa70facc5
Felix Leupold <felixleupold90@gmail.com>
2019-12-19 11:35:04 +0100

    deploying images on tags

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -2,21 +2,23 @@
 
 set -euo pipefail
 
+image_name=$1
+
 sudo apt-get update && sudo apt-get install -y python-pip && sudo pip install awscli
 
 # Get login token and execute login
 $(aws ecr get-login --no-include-email --region $AWS_REGION)
 
 echo "Tagging latest image with solver...";
-docker build --tag $REGISTRY_URI:$TRAVIS_BRANCH -f docker/rust/release/Dockerfile .
+docker build --tag $REGISTRY_URI:$image_name -f docker/rust/release/Dockerfile .
 
 echo "Pushing image";
-docker push $REGISTRY_URI:$TRAVIS_BRANCH
+docker push $REGISTRY_URI:$image_name
 
 echo "The image has been pushed";
 rm -rf .ssh/*
 
-if [ -n "$AUTODEPLOY_URL" ] && [ -n "$AUTODEPLOY_TOKEN" ]; then
+if [ "$image_name" == "master" ] && [ -n "$AUTODEPLOY_URL" ] && [ -n "$AUTODEPLOY_TOKEN" ]; then
     # Notifying webhook
     curl -s --output /dev/null --write-out "%{http_code}" \
     -H "Content-Type: application/json" \
