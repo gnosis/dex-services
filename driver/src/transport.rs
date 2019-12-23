8f89dd7bbad8ceca0ebe37a57c8b1b977cf3de09
Nicholas Rodrigues Lordello <nlordell@gmail.com>
2019-12-23 14:07:43 +0100

    Logging Transport (#375)
    Adds a logging transport implementation that logs JSON RPC requests and results to assist debugging.
    
    ### Test Plan
    
    CI and take a look at the dumped rinkeby logs. 
    
    ### Test Results
    
    Logs can be observed here: https://travis-ci.org/gnosis/dex-services/jobs/628688664#L434
    
    ### Commit History
    
    * added loging transport
    
    * better logging messages and log info
    
    * tweak error message
    
    * make snapp contract generic on transport

---
+++ driver/src/transport.rs
@@ -0,0 +1,79 @@
+use jsonrpc_core::types::request::Call;
+use log::{log, Level};
+use serde_json::Value;
+use web3::error::Error;
+use web3::futures::{Async, Future, Poll};
+use web3::{RequestId, Transport};
+
+/// A `Transport` wrapper that logs RPC messages
+#[derive(Clone, Debug)]
+pub struct LoggingTransport<T> {
+    inner: T,
+    level: Level,
+}
+
+impl<T> LoggingTransport<T>
+where
+    T: Transport,
+{
+    /// Create a new `LoggingTranport` from an underlying transport and log
+    /// level.
+    pub fn new(inner: T, level: Level) -> Self {
+        LoggingTransport { inner, level }
+    }
+}
+
+impl<T> Transport for LoggingTransport<T>
+where
+    T: Transport,
+{
+    type Out = LoggingFuture<T::Out>;
+
+    fn prepare(&self, method: &str, params: Vec<Value>) -> (RequestId, Call) {
+        self.inner.prepare(method, params)
+    }
+
+    fn send(&self, id: RequestId, request: Call) -> Self::Out {
+        log!(
+            self.level,
+            "sending request ID {}: {}",
+            id,
+            serde_json::to_string(&request).expect("request is invalid JSON")
+        );
+        LoggingFuture {
+            inner: self.inner.send(id, request),
+            id,
+            level: self.level,
+        }
+    }
+}
+
+/// A future that wraps JSON RPC results.
+pub struct LoggingFuture<F> {
+    inner: F,
+    id: RequestId,
+    level: Level,
+}
+
+impl<F> Future for LoggingFuture<F>
+where
+    F: Future<Item = Value, Error = Error>,
+{
+    type Item = Value;
+    type Error = Error;
+
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        let result = self.inner.poll();
+        match &result {
+            Ok(Async::Ready(ref value)) => log!(
+                self.level,
+                "request ID {} completed with result: {}",
+                self.id,
+                value
+            ),
+            Err(ref err) => log!(self.level, "request ID {} failed: {:?}", self.id, err),
+            _ => {}
+        }
+        result
+    }
+}
