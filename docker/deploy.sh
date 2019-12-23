5001f0e19dd74fc1eeb11142242b8ddaab776b75
Felix Leupold <felixleupold90@gmail.com>
2019-12-23 11:20:18 +0100

    [Deploy] Allow multiple services to be restarted on master commit

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -2,6 +2,15 @@
 
 set -euo pipefail
 
+function autodeploy {
+    # Notifying webhook
+    curl -s --output /dev/null --write-out "%{http_code}" \
+    -H "Content-Type: application/json" \
+    -X POST \
+    -d '{"token": "'$AUTODEPLOY_TOKEN'", "push_data": {"tag": "'$AUTODEPLOY_TAG'" }}' \
+    $1
+}
+
 image_name=$1
 
 sudo apt-get update && sudo apt-get install -y python-pip && sudo pip install awscli
@@ -18,11 +27,10 @@ docker push $REGISTRY_URI:$image_name
 echo "The image has been pushed";
 rm -rf .ssh/*
 
-if [ "$image_name" == "master" ] && [ -n "$AUTODEPLOY_URL" ] && [ -n "$AUTODEPLOY_TOKEN" ]; then
-    # Notifying webhook
-    curl -s --output /dev/null --write-out "%{http_code}" \
-    -H "Content-Type: application/json" \
-    -X POST \
-    -d '{"token": "'$AUTODEPLOY_TOKEN'", "push_data": {"tag": "'$AUTODEPLOY_TAG'" }}' \
-    $AUTODEPLOY_URL
+if [ "$image_name" == "master" ] && [ -n "$AUTODEPLOY_URLS" ] && [ -n "$AUTODEPLOY_TOKEN" ]; then
+    IFS=';'
+    read -ra AUTODEPLOY_URL_LIST <<< "$AUTODEPLOY_URLS"
+    for url in "${AUTODEPLOY_URL_LIST[@]}"; do
+      autodeploy "$url"
+    done
 fi
