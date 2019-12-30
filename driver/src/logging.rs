8ebc76c9e81ace1fdcd5efd27b85418550235ff9
Felix Leupold <felixleupold90@gmail.com>
2019-12-30 11:38:43 +0100

    [Logging] Custom Format that prints module (#390)
    * [Logging] Custom Format that prints module
    
    * fix clippy

--- driver/src/logging.rs
+++ driver/src/logging.rs
@@ -1,8 +1,8 @@
-use slog::{o, Drain, Logger};
+use slog::{o, Drain, Logger, OwnedKVList, Record};
 use slog_async::Async;
 use slog_envlogger::LogBuilder;
 use slog_scope::GlobalLoggerGuard;
-use slog_term::{CompactFormat, TermDecorator};
+use slog_term::{Decorator, TermDecorator};
 use std::env;
 
 /// The logging filter environment variable key.
@@ -15,9 +15,7 @@ const DEFAULT_FILTER: &str = "info";
 /// Initialize driver logging.
 pub fn init() -> (Logger, GlobalLoggerGuard) {
     let filter = env::var(FILTER_KEY).unwrap_or_else(|_| DEFAULT_FILTER.to_owned());
-    let format = CompactFormat::new(TermDecorator::new().stderr().build())
-        .build()
-        .fuse();
+    let format = CustomFormatter::new(TermDecorator::new().stderr().build()).fuse();
     let drain = Async::default(LogBuilder::new(format).parse(&filter).build());
     let logger = Logger::root(drain.fuse(), o!());
 
@@ -25,4 +23,49 @@ pub fn init() -> (Logger, GlobalLoggerGuard) {
     slog_stdlog::init().expect("failed to register logger");
 
     (logger, guard)
+}
+
+pub struct CustomFormatter<D: Decorator> {
+    decorator: D,
+}
+
+impl<D: Decorator> CustomFormatter<D> {
+    fn new(decorator: D) -> Self {
+        Self { decorator }
+    }
+}
+
+impl<D: Decorator> Drain for CustomFormatter<D> {
+    type Ok = ();
+    type Err = std::io::Error;
+    fn log(
+        &self,
+        record: &Record,
+        values: &OwnedKVList,
+    ) -> std::result::Result<Self::Ok, Self::Err> {
+        self.decorator.with_record(record, values, |mut decorator| {
+            decorator.start_timestamp()?;
+            slog_term::timestamp_utc(&mut decorator)?;
+
+            decorator.start_whitespace()?;
+            write!(decorator, " ")?;
+
+            decorator.start_level()?;
+            write!(decorator, "{}", record.level())?;
+
+            decorator.start_whitespace()?;
+            write!(decorator, " ")?;
+
+            write!(decorator, "[{}]", record.module())?;
+
+            decorator.start_whitespace()?;
+            write!(decorator, " ")?;
+
+            decorator.start_msg()?;
+            writeln!(decorator, "{}", record.msg())?;
+            decorator.flush()?;
+
+            Ok(())
+        })
+    }
 }
