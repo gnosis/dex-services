59a4c7d5c9e1a676a548f3d7de3a5bb70d4060cd
Nicholas Rodrigues Lordello <nlordell@gmail.com>
2020-01-27 15:48:09 +0100

    introduce new traits to make writing tests more concise

--- e2e/src/stablex.rs
+++ e2e/src/stablex.rs
@@ -6,7 +6,8 @@ use ethcontract::web3::types::{H160, U256};
 use ethcontract::Account;
 
 use crate::common::{
-    approve, create_accounts_with_funded_tokens, wait_for, FutureWaitExt, MAX_GAS, TOKEN_MINTED,
+    approve, create_accounts_with_funded_tokens, wait_for, FutureBuilderExt, FutureWaitExt,
+    MAX_GAS, TOKEN_MINTED,
 };
 
 pub fn setup_stablex(
@@ -17,33 +18,23 @@ pub fn setup_stablex(
     // Get all tokens but OWL in a generic way
     let (accounts, mut tokens) =
         create_accounts_with_funded_tokens(&web3, num_tokens - 1, num_users);
-    let mut instance = BatchExchange::deployed(&web3)
-        .wait()
-        .expect("Cannot get deployed BatchExchange");
+    let mut instance =
+        BatchExchange::deployed(&web3).wait_and_expect("Cannot get deployed BatchExchange");
     instance.defaults_mut().gas = Some(MAX_GAS.into());
     approve(&tokens, instance.address(), &accounts);
 
     // Set up OWL manually
     let owl_address = instance
         .token_id_to_address_map(0)
-        .call()
-        .wait()
-        .expect("Cannot get address of OWL Token");
+        .wait_and_expect("Cannot get address of OWL Token");
     let owl = TokenOWL::at(web3, owl_address);
-    owl.set_minter(accounts[0])
-        .send()
-        .wait()
-        .expect("Cannot set minter");
+    owl.set_minter(accounts[0]).wait_and_expect("Cannot set minter");
     for account in &accounts {
         owl.mint_owl(*account, U256::exp10(18) * TOKEN_MINTED)
-            .send()
-            .wait()
-            .expect("Cannot mint OWl");
+            .wait_and_expect("Cannot mint OWl");
         owl.approve(instance.address(), U256::exp10(18) * TOKEN_MINTED)
             .from(Account::Local(*account, None))
-            .send()
-            .wait()
-            .expect("Cannot approve OWL for burning");
+            .wait_and_expect("Cannot approve OWL for burning");
     }
 
     // token[0] is already added in constructor
@@ -51,9 +42,7 @@ pub fn setup_stablex(
         instance
             .add_token(token.address())
             .gas(MAX_GAS.into())
-            .send()
-            .wait()
-            .expect("Cannot add token");
+            .wait_and_expect("Cannot add token");
     }
     tokens.insert(0, IERC20::at(&web3, owl_address));
     (instance, accounts, tokens)
@@ -62,8 +51,6 @@ pub fn setup_stablex(
 pub fn close_auction(web3: &Web3<Http>, instance: &BatchExchange) {
     let seconds_remaining = instance
         .get_seconds_remaining_in_batch()
-        .call()
-        .wait()
-        .expect("Cannot get seconds remaining in batch");
+        .wait_and_expect("Cannot get seconds remaining in batch");
     wait_for(web3, seconds_remaining.as_u32());
 }
