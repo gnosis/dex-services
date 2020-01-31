97a04b5cfc8809e6c7f36031593175a87e7750d8
Felix Leupold <felixleupold90@gmail.com>
2020-01-31 11:18:04 +0100

    rebase

--- driver/src/solution_submission/mod.rs
+++ driver/src/solution_submission/mod.rs
@@ -1,12 +1,17 @@
+#![allow(clippy::ptr_arg)] // required for automock
+
 use crate::contracts::stablex_contract::StableXContract;
 use crate::error::DriverError;
 
 use dfusion_core::models::{Order, Solution};
+#[cfg(test)]
+use mockall::automock;
 
 use web3::types::U256;
 
 type Result<T> = std::result::Result<T, DriverError>;
 
+#[cfg_attr(test, automock)]
 pub trait StableXSolutionSubmitting {
     /// Return the objective value for the given solution in the given
     /// batch or an error.
@@ -68,65 +73,5 @@ impl<'a> StableXSolutionSubmitting for StableXSolutionSubmitter<'a> {
     ) -> Result<()> {
         self.contract
             .submit_solution(batch_index, orders, solution, claimed_objective_value)
-    }
-}
-
-#[cfg(test)]
-pub mod tests {
-    use super::*;
-    use crate::error::ErrorKind;
-    use mock_it::{Matcher, Mock};
-
-    type GetSolutionObjectiveValueArguments = (U256, Matcher<Vec<Order>>, Matcher<Solution>);
-    type SubmitSolutionArguments = (U256, Matcher<Vec<Order>>, Matcher<Solution>, Matcher<U256>);
-
-    #[derive(Clone)]
-    pub struct StableXSolutionSubmittingMock {
-        pub get_solution_objective_value: Mock<GetSolutionObjectiveValueArguments, Result<U256>>,
-        pub submit_solution: Mock<SubmitSolutionArguments, Result<()>>,
-    }
-
-    impl Default for StableXSolutionSubmittingMock {
-        fn default() -> Self {
-            Self {
-                get_solution_objective_value: Mock::new(Err(DriverError::new(
-                    "Unexpected call to get_solution_objective_value",
-                    ErrorKind::Unknown,
-                ))),
-                submit_solution: Mock::new(Err(DriverError::new(
-                    "Unexpected call to submit_solution",
-                    ErrorKind::Unknown,
-                ))),
-            }
-        }
-    }
-
-    impl StableXSolutionSubmitting for StableXSolutionSubmittingMock {
-        fn get_solution_objective_value(
-            &self,
-            batch_index: U256,
-            orders: Vec<Order>,
-            solution: Solution,
-        ) -> Result<U256> {
-            self.get_solution_objective_value.called((
-                batch_index,
-                Matcher::Val(orders),
-                Matcher::Val(solution),
-            ))
-        }
-        fn submit_solution(
-            &self,
-            batch_index: U256,
-            orders: Vec<Order>,
-            solution: Solution,
-            claimed_objective_value: U256,
-        ) -> Result<()> {
-            self.submit_solution.called((
-                batch_index,
-                Matcher::Val(orders),
-                Matcher::Val(solution),
-                Matcher::Val(claimed_objective_value),
-            ))
-        }
     }
 }
