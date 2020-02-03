6cddbd27d8b5f90e449d434120cb2c381245ed1d
Ben Smith <bh2smith@gmail.com>
2020-02-03 11:30:51 +0100

    Even more compact Auction Bid with doc string.

--- e2e/src/auction_bid.rs
+++ e2e/src/auction_bid.rs
@@ -1,15 +1,17 @@
 use ethcontract::web3::types::{H160, H256, U256};
 
+/// This is the AuctionBid as received from the EVM via smart contract query
+/// the slices are meant to be parsed as H256 (all other values require no alteration).
 type AuctionBidData = (
-    [u8; 32],
-    u64,
-    U256,
-    H160,
-    U256,
-    [u8; 32],
-    [u8; 32],
-    U256,
-    U256,
+    [u8; 32], // order_hash
+    u64,      // num_orders
+    U256,     // creation_timestamp
+    H160,     // solver (address)
+    U256,     // objective_value
+    [u8; 32], // tentative_state
+    [u8; 32], // solution_hash
+    U256,     // _auction_applied_time (always zero at time of bid)
+    U256,     // _applied_account_state_index (always 0 at time of bid)
 );
 
 #[derive(Debug)]
@@ -17,32 +19,14 @@ pub struct AuctionBid {
     data: AuctionBidData,
 }
 
-impl AuctionBid {
-    pub fn from(data: AuctionBidData) -> Self {
+impl From<AuctionBidData> for AuctionBid {
+    fn from(data: AuctionBidData) -> Self {
         AuctionBid { data }
     }
 }
 
 impl AuctionBid {
-    //    pub fn order_hash(&self) -> H256 {
-    //        H256::from_slice(&self.data.0)
-    //    }
-    //    pub fn num_orders(&self) -> u64 {
-    //        self.data.1
-    //    }
-    //    pub fn creation_time_stamp(&self) -> U256 {
-    //        self.data.2
-    //    }
-    //    pub fn solver(&self) -> H160 {
-    //        self.data.3
-    //    }
-    //    pub fn objective_value(&self) -> U256 {
-    //        self.data.4
-    //    }
     pub fn tentative_state(&self) -> H256 {
         H256::from_slice(&self.data.5)
     }
-    //    pub fn solution_hash(&self) -> H256 {
-    //        H256::from_slice(&self.data.6)
-    //    }
 }
