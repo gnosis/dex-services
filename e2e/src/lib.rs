1177b8e778f2c5036cdbaafedc2db1dbdfc732be
Nicholas Rodrigues Lordello <nlordell@gmail.com>
2020-02-06 14:50:46 +0100

    Remove Snapp Binaries And Tests (#491)
    * removed snapp driver, listnere and e2e tests
    
    * no longer copy dfusion binaries

--- e2e/src/lib.rs
+++ e2e/src/lib.rs
@@ -6,7 +6,5 @@ ethcontract::contract!("dex-contracts/build/contracts/IterableAppendOnlySet.json
 ethcontract::contract!("dex-contracts/build/contracts/TokenOWL.json");
 ethcontract::contract!("dex-contracts/build/contracts/ERC20Mintable.json");
 
-pub mod auction_bid;
 pub mod common;
-pub mod snapp;
 pub mod stablex;
