6917bf55b8ce1145805d4f1cca02f3d49b453c93
Benjamin Smith <bh2smith@users.noreply.github.com>
2020-02-10 22:42:31 +0100

    Revert "433 batched orders (#484)"
    This reverts commit 1827e2ddefb6513e38f37502fcef1d0fe6c420ad.

--- driver/src/main.rs
+++ driver/src/main.rs
@@ -10,10 +10,10 @@ mod solution_submission;
 mod transport;
 mod util;
 
-use crate::contracts::{stablex_contract::BatchExchange, web3_provider};
+use crate::contracts::stablex_contract::BatchExchange;
 use crate::driver::stablex_driver::StableXDriver;
 use crate::metrics::{MetricsServer, StableXMetrics};
-use crate::orderbook::{FilteredOrderbookReader, PaginatedStableXOrderBookReader};
+use crate::orderbook::{FilteredOrderbookReader, StableXOrderBookReader};
 use crate::price_finding::price_finder_interface::OptimizationModel;
 use crate::price_finding::Fee;
 use crate::solution_submission::StableXSolutionSubmitter;
@@ -26,18 +26,6 @@ use std::env;
 use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
-
-fn auction_data_batch_size() -> u64 {
-    const KEY: &str = "AUCTION_DATA_BATCH_SIZE";
-    const DEFAULT: u64 = 100;
-    env::var(KEY)
-        .map(|str| {
-            str.parse()
-                .map_err(|err| format!("couldn't parse {} environment variable: {}", KEY, err))
-                .unwrap()
-        })
-        .unwrap_or(DEFAULT)
-}
 
 fn main() {
     let (_, _guard) = logging::init();
@@ -54,8 +42,7 @@ fn main() {
         env::var("OPTIMIZATION_MODEL").unwrap_or_else(|_| String::from("NAIVE"));
     let optimization_model = OptimizationModel::from(optimization_model_string.as_str());
 
-    let (web3, _event_loop_handle) = web3_provider(&ethereum_node_url).unwrap();
-    let contract = BatchExchange::new(&web3, network_id).unwrap();
+    let (contract, _event_loop) = BatchExchange::new(ethereum_node_url, network_id).unwrap();
     info!("Using contract at {}", contract.address());
     info!("Using account {}", contract.account());
 
@@ -70,8 +57,7 @@ fn main() {
     let fee = Some(Fee::default());
     let mut price_finder = util::create_price_finder(fee, optimization_model);
 
-    let orderbook =
-        PaginatedStableXOrderBookReader::new(&contract, auction_data_batch_size(), &web3);
+    let orderbook = StableXOrderBookReader::new(&contract);
     let parsed_filter = serde_json::from_str(&filter)
         .map_err(|e| {
             error!("Error parsing orderbook filter: {}", &e);
