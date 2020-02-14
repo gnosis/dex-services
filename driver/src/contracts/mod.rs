cd62eb92bbfeb9c7e8e7a27924fafd6a1e6c3280
Nicholas Rodrigues Lordello <nlordell@gmail.com>
2020-02-14 22:05:21 +0100

    ergonomics

--- driver/src/contracts/mod.rs
+++ driver/src/contracts/mod.rs
@@ -5,14 +5,13 @@ use crate::error::DriverError;
 use crate::transport::HttpTransport;
 use ethcontract::contract::MethodDefaults;
 use ethcontract::{Account, PrivateKey};
-use log::Level;
 use std::env;
 use std::time::Duration;
 
 pub type Web3 = ethcontract::web3::api::Web3<HttpTransport>;
 
 pub fn web3_provider(url: &str) -> Result<Web3, DriverError> {
-    let http = HttpTransport::new(url, Level::Debug, Duration::from_secs(10))?;
+    let http = HttpTransport::new(url, Duration::from_secs(10))?;
     let web3 = Web3::new(http);
 
     Ok(web3)
--- driver/src/transport.rs
+++ driver/src/transport.rs
@@ -6,7 +6,7 @@ use futures::compat::Compat;
 use futures::future::{BoxFuture, FutureExt, TryFutureExt};
 use isahc::config::VersionNegotiation;
 use isahc::prelude::{HttpClient, Request, ResponseExt};
-use log::{log, warn, Level};
+use log::{debug, warn};
 use serde::Deserialize;
 use serde_json::Value;
 use std::fmt::{self, Debug, Formatter};
@@ -21,18 +21,13 @@ pub struct HttpTransport(Arc<HttpTransportInner>);
 
 struct HttpTransportInner {
     url: String,
-    log_level: Level,
     client: HttpClient,
     id: AtomicUsize,
 }
 
 impl HttpTransport {
     /// Creates a new HTTP transport with settings.
-    pub fn new(
-        url: impl Into<String>,
-        log_level: Level,
-        timeout: Duration,
-    ) -> Result<HttpTransport, DriverError> {
+    pub fn new(url: impl Into<String>, timeout: Duration) -> Result<HttpTransport, DriverError> {
         let client = HttpClient::builder()
             .timeout(timeout)
             .version_negotiation(VersionNegotiation::http11())
@@ -40,7 +35,6 @@ impl HttpTransport {
 
         Ok(HttpTransport(Arc::new(HttpTransportInner {
             url: url.into(),
-            log_level,
             client,
             id: AtomicUsize::default(),
         })))
@@ -48,27 +42,21 @@ impl HttpTransport {
 }
 
 impl HttpTransportInner {
+    /// Execute an HTTP JSON RPC request.
     async fn execute(self: Arc<Self>, id: RequestId, request: Call) -> Result<Value, Web3Error> {
         let request = serde_json::to_string(&request)?;
-        log!(
-            self.log_level,
-            "[id:{}] sending request: '{}'",
-            id,
-            &request,
-        );
+        debug!("[id:{}] sending request: '{}'", id, &request,);
 
         let http_request = Request::post(&self.url)
             .header("Content-Type", "application/json")
             .body(request)
-            .map_err(|err| Web3Error::Transport(err.to_string()))?;
+            .map_err(transport_err)?;
         let mut response = self
             .client
             .send_async(http_request)
             .await
-            .map_err(|err| Web3Error::Transport(err.to_string()))?;
-        let body = response
-            .text()
-            .map_err(|err| Web3Error::Transport(err.to_string()))?;
+            .map_err(transport_err)?;
+        let body = response.text().map_err(transport_err)?;
 
         if !response.status().is_success() {
             warn!(
@@ -84,7 +72,7 @@ impl HttpTransportInner {
                 body.trim(),
             )));
         }
-        log!(self.log_level, "received response ID {}: '{}'", id, &body);
+        debug!("[id:{}] received response: '{}'", id, &body);
 
         let mut json = Value::from_str(&body)?;
         if let Some(map) = json.as_object_mut() {
@@ -123,4 +111,9 @@ impl Transport for HttpTransport {
     fn send(&self, id: RequestId, request: Call) -> Self::Out {
         self.0.clone().execute(id, request).boxed().compat()
     }
+}
+
+/// Error conversion method for wrapping an HTTP error in a `web3` error.
+fn transport_err(err: impl std::error::Error) -> Web3Error {
+    Web3Error::Transport(err.to_string())
 }
