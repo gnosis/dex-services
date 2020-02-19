ef92d8959c281f6b5b0d7b6d88f8e318d0b04bf3
josojo <josojo@hotmail.de>
2020-02-19 10:36:47 +0530

    small test improvement

--- driver/src/price_finding/optimization_price_finder.rs
+++ driver/src/price_finding/optimization_price_finder.rs
@@ -678,20 +678,39 @@ pub mod tests {
             decimals: 18,
             external_price: 1000000000000000000,
         });
-        token_data.insert(1, token_info_1);
+        token_data.insert(2, token_info_1);
 
+        let orders = [
+            models::Order {
+                id: 0,
+                account_id: H160::from_low_u64_be(0),
+                sell_token: 1,
+                buy_token: 2,
+                sell_amount: 100,
+                buy_amount: 200,
+            },
+            models::Order {
+                id: 0,
+                account_id: H160::from_low_u64_be(1),
+                sell_token: 2,
+                buy_token: 1,
+                sell_amount: 200,
+                buy_amount: 100,
+            },
+        ]
+        .to_vec();
         let input = solver_input::Input {
             // tokens should also end up sorted in the end
-            tokens: token_data,
+            tokens: serialize_tokens(&orders, token_data),
             ref_token: "T0000".to_owned(),
             accounts,
-            orders: vec![],
+            orders: orders.iter().map(serialize_order).collect(),
             fee: None,
         };
         let result = serde_json::to_string(&input).expect("Unable to serialize account state");
         assert_eq!(
             result,
-            r#"{"tokens":{"T0001":{"decimals":18,"externalPrice":1000000000000000000}},"refToken":"T0000","accounts":{"13a0b42b9c180065510615972858bf41d1972a55":{},"4fd7c947ca0aba9d8678885e2b8c4d6a4e946984":{"T0000":"100","T0001":"100","T0002":"100","T0003":"100"},"52a67f22d628c84c1f1e73ebb0e9ae272e302dd9":{}},"orders":[],"fee":null}"#
+            r#"{"tokens":{"T0001":null,"T0002":{"decimals":18,"externalPrice":1000000000000000000}},"refToken":"T0000","accounts":{"13a0b42b9c180065510615972858bf41d1972a55":{},"4fd7c947ca0aba9d8678885e2b8c4d6a4e946984":{"T0000":"100","T0001":"100","T0002":"100","T0003":"100"},"52a67f22d628c84c1f1e73ebb0e9ae272e302dd9":{}},"orders":[{"accountID":"0000000000000000000000000000000000000000","sellToken":"T0001","buyToken":"T0002","sellAmount":"100","buyAmount":"200"},{"accountID":"0000000000000000000000000000000000000001","sellToken":"T0002","buyToken":"T0001","sellAmount":"200","buyAmount":"100"}],"fee":null}"#
         );
     }
 }
