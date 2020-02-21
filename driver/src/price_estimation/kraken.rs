e31783392f503987acf64ed0736824bd3eedcad0
Nicholas Rodrigues Lordello <nlordell@gmail.com>
2020-02-21 20:14:07 +0100

    small fix

--- driver/src/price_estimation/kraken.rs
+++ driver/src/price_estimation/kraken.rs
@@ -50,7 +50,7 @@ where
         let token_assets = tokens
             .iter()
             .flat_map(|token| {
-                let asset = find_asset(&token.symbol(), &assets)?;
+                let asset = find_asset(token.symbol(), &assets)?;
                 let pair = find_asset_pair(asset, usd, &asset_pairs)?;
                 Some((pair.to_owned(), token))
             })
