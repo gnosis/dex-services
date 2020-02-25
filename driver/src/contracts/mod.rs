fb9bcac144354d5e7b2a91879f417e0bc44f7092
Felix Leupold <felixleupold90@gmail.com>
2020-02-25 14:35:38 +0100

    Using enum instead of reason string

--- driver/src/contracts/mod.rs
+++ driver/src/contracts/mod.rs
@@ -2,11 +2,13 @@ pub mod stablex_auction_element;
 pub mod stablex_contract;
 
 use crate::transport::HttpTransport;
-use anyhow::{Error, Result};
+use anyhow::{anyhow, Error, Result};
 use ethcontract::contract::MethodDefaults;
 use ethcontract::errors::{ExecutionError, MethodError};
 
 use ethcontract::{Account, PrivateKey};
+use std::convert::TryFrom;
+use std::fmt;
 use std::time::Duration;
 
 pub type Web3 = ethcontract::web3::api::Web3<HttpTransport>;
@@ -29,21 +31,45 @@ fn method_defaults(key: PrivateKey, network_id: u64) -> Result<MethodDefaults> {
     Ok(defaults)
 }
 
-const EXPECTED_ERRORS: &[&str; 3] = &[
-    "New objective doesn\'t sufficiently improve current solution",
-    "Claimed objective doesn't sufficiently improve current solution",
-    "SafeMath: subtraction overflow",
-];
+pub enum BenignSolutionFailure {
+    BetterSolutionAlreadySubmitted,
+    NegativeUtility,
+}
 
-pub fn extract_benign_submission_failure(err: &Error) -> Option<String> {
+impl fmt::Display for BenignSolutionFailure {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            BenignSolutionFailure::BetterSolutionAlreadySubmitted => {
+                write!(f, "Better solution already submitted")
+            }
+            BenignSolutionFailure::NegativeUtility => write!(f, "Negative Utility"),
+        }
+    }
+}
+
+impl TryFrom<&str> for BenignSolutionFailure {
+    type Error = anyhow::Error;
+
+    fn try_from(reason: &str) -> Result<Self, Self::Error> {
+        match reason {
+            "New objective doesn\'t sufficiently improve current solution" => {
+                Ok(BenignSolutionFailure::BetterSolutionAlreadySubmitted)
+            }
+            "Claimed objective doesn't sufficiently improve current solution" => {
+                Ok(BenignSolutionFailure::BetterSolutionAlreadySubmitted)
+            }
+            "SafeMath: subtraction overflow" => Ok(BenignSolutionFailure::NegativeUtility),
+            _ => Err(anyhow!("Unexpected error")),
+        }
+    }
+}
+
+pub fn extract_benign_submission_failure(err: &Error) -> Option<BenignSolutionFailure> {
     err.downcast_ref::<MethodError>()
         .and_then(|method_error| match &method_error.inner {
             ExecutionError::Revert(Some(reason)) => {
-                if EXPECTED_ERRORS.contains(&&reason[..]) {
-                    Some(reason.clone())
-                } else {
-                    None
-                }
+                let reason_slice: &str = &*reason;
+                BenignSolutionFailure::try_from(reason_slice).ok()
             }
             _ => None,
         })
