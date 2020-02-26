7db96e868b7e955910fdf2a590fc717007788c5a
Felix Leupold <felixleupold90@gmail.com>
2020-02-26 15:45:02 +0100

    [Easy] Remove unused code (#590)
    This enum creeped in from #588 (it was part of an earlier draft)
    
    Kill it with 🔥 
    
    ### Test Plan
    
    CI

--- driver/src/contracts/mod.rs
+++ driver/src/contracts/mod.rs
@@ -2,12 +2,10 @@ pub mod stablex_auction_element;
 pub mod stablex_contract;
 
 use crate::transport::HttpTransport;
-use anyhow::{anyhow, Result};
+use anyhow::Result;
 use ethcontract::contract::MethodDefaults;
 
 use ethcontract::{Account, PrivateKey};
-use std::convert::TryFrom;
-use std::fmt;
 use std::time::Duration;
 
 pub type Web3 = ethcontract::web3::api::Web3<HttpTransport>;
@@ -28,37 +26,4 @@ fn method_defaults(key: PrivateKey, network_id: u64) -> Result<MethodDefaults> {
     };
 
     Ok(defaults)
-}
-
-pub enum BenignSolutionFailure {
-    BetterSolutionAlreadySubmitted,
-    NegativeUtility,
-}
-
-impl fmt::Display for BenignSolutionFailure {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            BenignSolutionFailure::BetterSolutionAlreadySubmitted => {
-                write!(f, "Better solution already submitted")
-            }
-            BenignSolutionFailure::NegativeUtility => write!(f, "Negative Utility"),
-        }
-    }
-}
-
-impl TryFrom<&str> for BenignSolutionFailure {
-    type Error = anyhow::Error;
-
-    fn try_from(reason: &str) -> Result<Self, Self::Error> {
-        match reason {
-            "New objective doesn\'t sufficiently improve current solution" => {
-                Ok(BenignSolutionFailure::BetterSolutionAlreadySubmitted)
-            }
-            "Claimed objective doesn't sufficiently improve current solution" => {
-                Ok(BenignSolutionFailure::BetterSolutionAlreadySubmitted)
-            }
-            "SafeMath: subtraction overflow" => Ok(BenignSolutionFailure::NegativeUtility),
-            _ => Err(anyhow!("Unexpected error")),
-        }
-    }
 }
