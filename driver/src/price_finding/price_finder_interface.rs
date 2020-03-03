aaa328d471e98d55c3207a34ed6a1719b6e4bcda
josojo <josojo@hotmail.de>
2020-03-03 16:43:25 +0100

    reseting flag --useExternalPrices, as this will indicate the solver to use the drivers prices later

--- driver/src/price_finding/price_finder_interface.rs
+++ driver/src/price_finding/price_finder_interface.rs
@@ -46,6 +46,7 @@ impl SolverConfig {
             SolverConfig::FallbackSolver { solver_time_limit } => vec![
                 format!("--solverTimeLimit={:}", solver_time_limit),
                 String::from("--tokenInfo=/app/batchauctions/scripts/token_info_mainnet.json"),
+                String::from("--useExternalPrices"),
             ],
             SolverConfig::NaiveSolver => {
                 panic!("OptimizationSolver should not be called with naive solver")
