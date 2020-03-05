acd7573b79bca3539936bb2437b67ea09c1c84ca
josojo <josojo@hotmail.de>
2020-03-05 14:15:20 +0100

    removing the tokeninfo for solver, as it will not longer be needed

--- driver/src/price_finding/price_finder_interface.rs
+++ driver/src/price_finding/price_finder_interface.rs
@@ -45,7 +45,6 @@ impl SolverConfig {
             }
             SolverConfig::FallbackSolver { solver_time_limit } => vec![
                 format!("--solverTimeLimit={:}", solver_time_limit),
-                String::from("--tokenInfo=/app/batchauctions/scripts/token_info_mainnet.json"),
                 String::from("--useExternalPrices"),
             ],
             SolverConfig::NaiveSolver => {
