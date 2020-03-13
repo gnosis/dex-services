6cd0b3aa0d37ca827c0c851b226b56d458cddd86
Valentin Kettner <vakevk+git@gmail.com>
2020-03-13 14:47:00 +0100

    temp

--- e2e/tests/stablex_test.rs
+++ e2e/tests/stablex_test.rs
@@ -2,10 +2,12 @@
 fn test_rinkeby() {
     std::thread::sleep(std::time::Duration::from_secs(10));
     // Make sure there was no error
-    let output = std::process::Command::new("docker-compose")
+    let output = std::process::Command::new("docker")
         .arg("logs")
+        .arg("dex-services_stablex_1")
         .output()
         .expect("failed to execute process");
+    assert!(output.status.success());
     // Errors go to stderr while other messages go to stdout.
     let logs = String::from_utf8(output.stderr).expect("failed to read logs");
     assert!(logs.is_empty());
