397b2781e9a4cba07e98849531d78320135a6584
Valentin Kettner <vakevk+git@gmail.com>
2020-03-13 13:54:17 +0100

    temp

--- driver/src/main.rs
+++ driver/src/main.rs
@@ -142,6 +142,7 @@ struct Options {
 fn main() {
     let options = Options::from_args();
     let (_, _guard) = logging::init(&options.log_filter);
+    log::error("aaaaaaaaaa errror aaaaaaaaa");
     info!("Starting driver with runtime options: {:#?}", options);
     let solver_config = SolverConfig::new(&options.solver_type, options.solver_time_limit).unwrap();
 
