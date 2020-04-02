9610a80f5183e7b6eba8e44d4e53a207bffb29a8
josojo <josojo@hotmail.de>
2020-04-02 13:44:11 +0200

    adding deploy scripts

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -9,22 +9,31 @@ sudo apt-get update && sudo apt-get install -y python-pip && sudo pip install aw
 # Get login token and execute login
 $(aws ecr get-login --no-include-email --region $AWS_REGION)
 
-echo "Tagging latest image with solver...";
-docker build --tag $REGISTRY_URI:$image_name -f docker/rust/release/Dockerfile .
+echo "Tagging latest private image with solver...";
+docker build --tag $REGISTRY_URI:$image_name --build-arg BINARY_BASE=stablex-binary-private -f docker/rust/release/Dockerfile .
 
-echo "Pushing image";
+echo "Pushing private image";
 docker push $REGISTRY_URI:$image_name
 
-echo "The image has been pushed";
+echo "The private image has been pushed";
 rm -rf .ssh/*
+
+echo "Docker login"
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin;
+
+echo "Pushing public image to Docker-hub";
+docker build --tag gnosispm/dex-services:$image_name --build-arg BINARY_BASE=stablex-binary-public -f docker/Dockerfile .
+
+docker push gnosispm/dex-services:$image_name;
+echo "The image has been pushed";
 
 if [ "$image_name" == "master" ] && [ -n "$AUTODEPLOY_URL" ] && [ -n "$AUTODEPLOY_TOKEN" ]; then
     # Notifying webhook
     curl -s  \
-      --output /dev/null \
-      --write-out "%{http_code}" \
-      -H "Content-Type: application/json" \
-      -X POST \
-      -d '{"token": "'$AUTODEPLOY_TOKEN'", "push_data": {"tag": "'$AUTODEPLOY_TAG'" }}' \
-      $AUTODEPLOY_URL
+    --output /dev/null \
+    --write-out "%{http_code}" \
+    -H "Content-Type: application/json" \
+    -X POST \
+    -d '{"token": "'$AUTODEPLOY_TOKEN'", "push_data": {"tag": "'$AUTODEPLOY_TAG'" }}' \
+    $AUTODEPLOY_URL
 fi
