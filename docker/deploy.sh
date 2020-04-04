d0fed59ac4781f6e1570753bf5bfa0a9e9ecb2ee
josojo <josojo@hotmail.de>
2020-04-04 14:51:18 +0200

    adding forgotten files

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -9,21 +9,22 @@ sudo apt-get update && sudo apt-get install -y python-pip && sudo pip install aw
 # Get login token and execute login
 $(aws ecr get-login --no-include-email --region $AWS_REGION)
 
-echo "Tagging latest private image with solver...";
-docker build --tag $REGISTRY_URI:$image_name --build-arg -f docker/rust/release/Dockerfile .
+echo "Tagging latest image with solver...";
+docker build --tag $REGISTRY_URI:$image_name -f docker/rust/release/Dockerfile .
 
-echo "Pushing private image";
+echo "Pushing image";
 docker push $REGISTRY_URI:$image_name
 
 echo "The image has been pushed";
+rm -rf .ssh/*
 
 if [ "$image_name" == "master" ] && [ -n "$AUTODEPLOY_URL" ] && [ -n "$AUTODEPLOY_TOKEN" ]; then
     # Notifying webhook
     curl -s  \
-    --output /dev/null \
-    --write-out "%{http_code}" \
-    -H "Content-Type: application/json" \
-    -X POST \
-    -d '{"token": "'$AUTODEPLOY_TOKEN'", "push_data": {"tag": "'$AUTODEPLOY_TAG'" }}' \
-    $AUTODEPLOY_URL
+      --output /dev/null \
+      --write-out "%{http_code}" \
+      -H "Content-Type: application/json" \
+      -X POST \
+      -d '{"token": "'$AUTODEPLOY_TOKEN'", "push_data": {"tag": "'$AUTODEPLOY_TAG'" }}' \
+      $AUTODEPLOY_URL
 fi
