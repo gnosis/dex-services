798a45045b6b3ee65cbcb1bd4c9b391da59248f9
Valentin Kettner <vakevk+git@gmail.com>
2020-04-06 13:03:44 +0200

    Remove unused struct

--- driver/src/orderbook/streamed/mod.rs
+++ driver/src/orderbook/streamed/mod.rs
@@ -9,14 +9,3 @@ type TokenAddress = Address;
 type OrderId = u16;
 type TokenId = u16;
 type BatchId = u32;
-
-#[cfg(test)]
-mod tests {
-    use serde::{Deserialize, Serialize};
-
-    #[derive(Debug, Deserialize, Serialize)]
-    pub struct EventBackup {
-        pub event: crate::contracts::stablex_contract::batch_exchange::Event,
-        pub block_timestamp: u64,
-    }
-}
