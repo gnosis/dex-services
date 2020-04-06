7d5da0c8beccbc4c167e90991655f89ff3e80561
Valentin Kettner <vakevk+git@gmail.com>
2020-04-06 13:03:00 +0200

    Fix streamed module not getting compiled

--- driver/src/orderbook/mod.rs
+++ driver/src/orderbook/mod.rs
@@ -10,6 +10,7 @@ use std::convert::TryInto;
 
 mod filtered_orderbook;
 mod paginated_auction_data_reader;
+mod streamed;
 pub use filtered_orderbook::{FilteredOrderbookReader, OrderbookFilter};
 
 #[cfg_attr(test, automock)]
