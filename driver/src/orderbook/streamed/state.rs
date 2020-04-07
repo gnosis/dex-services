34d3a1315e19d23ccc12bddc9f033907452a35fd
Valentin Kettner <vakevk+git@gmail.com>
2020-04-07 11:22:48 +0200

    Handle trades and solutions

--- driver/src/orderbook/streamed/state.rs
+++ driver/src/orderbook/streamed/state.rs
@@ -11,36 +11,51 @@ use std::collections::HashMap;
 use std::iter::Iterator;
 use thiserror::Error;
 
-// TODO: Should we handle https://github.com/gnosis/dex-contracts/issues/620 ?
-// There is a workaround detailed in the issue it hasn't been implemented.
-
 // Most types, fields, functions in this module mirror the smart contract because we need to
 // emulate what it does based on the events it emits.
+//
+// We keep track of the pending solution for the batch separately instead of applying the trades
+// immediately. This ensures that orders and balances have correctly *not* been updated by the
+// pending solution until the solution becomes the real solution.
+// This has the side effect of simplifying solution revert logic by allowing us to replace the whole
+// pending solution instead of having to revert individual trades.
 
 /// The orderbook state as built from received events.
 ///
 /// Note that there is no way to revert an event. The order in which events are received matters
 /// Applying events `A B` does not always result in the same state as `B A`. For example, a Trade
 /// can only be processed if the OrderPlacement for the traded order has previously been observed.
-#[derive(Clone, Debug, Default, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct State {
     orders: HashMap<(UserId, OrderId), Order>,
     balances: HashMap<(UserId, TokenAddress), Balance>,
     tokens: Tokens,
+    pending_solution: PendingSolution,
 }
 
 impl State {
+    pub fn new() -> Self {
+        Self {
+            orders: HashMap::new(),
+            balances: HashMap::new(),
+            tokens: Tokens(Vec::new()),
+            pending_solution: PendingSolution::AccumulatingTrades(Vec::new()),
+        }
+    }
+
     /// Can be used to create a `crate::models::AccountState`.
     pub fn account_state(
-        &self,
+        &mut self,
         batch_id: BatchId,
     ) -> impl Iterator<Item = ((UserId, TokenId), u128)> + '_ {
+        self.apply_pending_solution_if_needed(batch_id);
+        let tokens = &self.tokens;
         self.balances
             .iter()
             .filter_map(move |((user_id, token_address), balance)| {
                 // It is possible that a user has a balance for a token that hasn't been added to
                 // the exchange because tokens can be deposited anyway.
-                let token_id = self.tokens.get_id_by_address(*token_address)?;
+                let token_id = tokens.get_id_by_address(*token_address)?;
                 Some((
                     (*user_id, token_id),
                     balance.current_balance(batch_id).low_u128(),
@@ -48,10 +63,11 @@ impl State {
             })
     }
 
-    pub fn orders(&self, batch_id: BatchId) -> impl Iterator<Item = ModelOrder> + '_ {
+    pub fn orders(&mut self, batch_id: BatchId) -> impl Iterator<Item = ModelOrder> + '_ {
+        self.apply_pending_solution_if_needed(batch_id);
         self.orders
             .iter()
-            .filter(move |(_, order)| order.valid_from <= batch_id && batch_id <= order.valid_until)
+            .filter(move |(_, order)| order.is_valid_in_batch(batch_id))
             .map(|((user_id, order_id), order)| {
                 let (buy_amount, sell_amount) = stablex_auction_element::compute_buy_sell_amounts(
                     order.price_numerator,
@@ -83,6 +99,7 @@ impl State {
     /// `block_batch_id` is the current batch based on the timestamp of the block that contains the
     ///  event.
     pub fn apply_event(&mut self, event: &Event, block_batch_id: BatchId) -> Result<(), Error> {
+        self.apply_pending_solution_if_needed(block_batch_id);
         match event {
             Event::Deposit(event) => self.deposit(event, block_batch_id),
             Event::WithdrawRequest(event) => self.withdraw_request(event),
@@ -90,10 +107,12 @@ impl State {
             Event::TokenListing(event) => self.token_listing(event),
             Event::OrderPlacement(event) => self.order_placement(event),
             Event::OrderCancellation(event) => self.order_cancellation(event, block_batch_id),
-            Event::OrderDeletion(event) => self.order_deletion(event),
-            Event::Trade(_event) => unimplemented!(),
-            Event::TradeReversion(_event) => unimplemented!(),
-            Event::SolutionSubmission(_event) => unimplemented!(),
+            Event::OrderDeletion(event) => self.order_deletion(event, block_batch_id),
+            Event::Trade(event) => self.trade(event),
+            // No need to do anything. The solution will be reverted by the first trade of the next
+            // solution.
+            Event::TradeReversion(_) => Ok(()),
+            Event::SolutionSubmission(event) => self.solution_submission(event, block_batch_id),
         }
     }
 
@@ -158,10 +177,149 @@ impl State {
         Ok(())
     }
 
-    fn order_deletion(&mut self, event: &OrderDeletion) -> Result<(), Error> {
+    fn order_deletion(
+        &mut self,
+        event: &OrderDeletion,
+        block_batch_id: BatchId,
+    ) -> Result<(), Error> {
+        if let Some(order) = self.orders.get(&(event.owner, event.id)) {
+            if order.is_valid_in_batch(block_batch_id - 1) {
+                return Err(Error::DeletingValidOrder);
+            } else {
+                self.orders.remove(&(event.owner, event.id));
+            }
+        }
         // Orders are allowed to be deleted multiple times.
-        self.orders.remove(&(event.owner, event.id));
         Ok(())
+    }
+
+    fn trade(&mut self, event: &Trade) -> Result<(), Error> {
+        if [event.sell_token, event.buy_token]
+            .iter()
+            .any(|token| self.tokens.get_address_by_id(*token).is_none())
+        {
+            return Err(Error::UnknownToken);
+        }
+        if self.orders.get(&(event.owner, event.order_id)).is_none() {
+            return Err(Error::UnknownOrder);
+        }
+        match &mut self.pending_solution {
+            PendingSolution::AccumulatingTrades(trades) => trades.push(event.clone()),
+            // If there has been a solution before and we get a new trade then the previous solution
+            // must have been reverted.
+            PendingSolution::Submitted { .. } => {
+                self.pending_solution = PendingSolution::AccumulatingTrades(vec![event.clone()])
+            }
+        };
+        Ok(())
+    }
+
+    fn solution_submission(
+        &mut self,
+        event: &SolutionSubmission,
+        block_batch_id: BatchId,
+    ) -> Result<(), Error> {
+        if self.tokens.get_address_by_id(0).is_none() {
+            return Err(Error::SolutionWithoutFeeToken);
+        }
+        let trades = match &mut self.pending_solution {
+            PendingSolution::AccumulatingTrades(trades) => std::mem::replace(trades, Vec::new()),
+            // This would be weird because it means that the previous solution had no trades. Not
+            // sure if we should error.
+            PendingSolution::Submitted { .. } => Vec::new(),
+        };
+        self.pending_solution = PendingSolution::Submitted {
+            batch_id: block_batch_id,
+            submitter: event.submitter,
+            burnt_fees: event.burnt_fees,
+            trades,
+        };
+        Ok(())
+    }
+
+    fn apply_pending_solution_if_needed(&mut self, block_batch_id: BatchId) {
+        let trades = match &mut self.pending_solution {
+            PendingSolution::Submitted {
+                batch_id,
+                submitter,
+                burnt_fees,
+                trades,
+            } if *batch_id < block_batch_id => {
+                // Cannot fail because we ensure there is a fee token when a solution event is
+                // received.
+                let fee_token = self.tokens.get_address_by_id(0).unwrap();
+                let balance = self.balances.entry((*submitter, fee_token)).or_default();
+                balance.balance += *burnt_fees;
+                // This looks weird but we need to call self.apply_trade outside of the match to
+                // prevent multiple mut self borrows.
+                let trades = std::mem::replace(trades, Vec::new());
+                self.pending_solution = PendingSolution::AccumulatingTrades(Vec::new());
+                trades
+            }
+            _ => return,
+        };
+        for trade in trades {
+            // Cannot fail because we ensure that the tokens exist when a trade event is
+            // received and that the order exists and doesn't get removed while it is valid.
+            self.apply_trade(
+                trade.owner,
+                trade.order_id,
+                trade.executed_sell_amount,
+                trade.executed_buy_amount,
+                block_batch_id,
+            )
+            .unwrap();
+        }
+    }
+
+    /// Fails if any tokens or order don't exist.
+    fn apply_trade(
+        &mut self,
+        user: UserId,
+        order: OrderId,
+        executed_sell_amount: u128,
+        executed_buy_amount: u128,
+        batch_id: BatchId,
+    ) -> Result<(), Error> {
+        let order = self
+            .orders
+            .get_mut(&(user, order))
+            .ok_or(Error::UnknownOrder)?;
+        let sell_token = self
+            .tokens
+            .get_address_by_id(order.sell_token)
+            .ok_or(Error::UnknownToken)?;
+        let buy_token = self
+            .tokens
+            .get_address_by_id(order.buy_token)
+            .ok_or(Error::UnknownToken)?;
+
+        if order.has_limited_amount() {
+            order.used_amount += executed_sell_amount;
+        }
+        self.apply_balance_from_trade(user, sell_token, executed_sell_amount, batch_id, true);
+        self.apply_balance_from_trade(user, buy_token, executed_buy_amount, batch_id, false);
+        Ok(())
+    }
+
+    fn apply_balance_from_trade(
+        &mut self,
+        user: UserId,
+        token: TokenAddress,
+        executed_amount: u128,
+        batch_id: BatchId,
+        subtract: bool,
+    ) {
+        let balance = self.balances.entry((user, token)).or_default();
+        balance.update_deposit_balance(batch_id);
+        // It possible that we get an underflow here because the user did not have enough
+        // deposited for the sale but in the same batch filled another order that increased their
+        // balance but whose trade event we have not yet processed.
+        balance.balance = if subtract {
+            balance.balance.overflowing_sub(executed_amount.into()).0
+        } else {
+            balance.balance.overflowing_add(executed_amount.into()).0
+        };
     }
 }
 
@@ -175,6 +333,10 @@ pub enum Error {
     OrderAlreadyExists,
     #[error("math underflow")]
     MathUnderflow,
+    #[error("solution submitted but there is no fee token")]
+    SolutionWithoutFeeToken,
+    #[error("attempt to delete an order that is still valid")]
+    DeletingValidOrder,
 }
 
 /// Bidirectional map between token id and token address.
@@ -213,6 +375,10 @@ struct Order {
 impl Order {
     fn has_limited_amount(&self) -> bool {
         self.price_numerator != std::u128::MAX && self.price_denominator != std::u128::MAX
+    }
+
+    fn is_valid_in_batch(&self, batch_id: BatchId) -> bool {
+        self.valid_from <= batch_id && batch_id <= self.valid_until
     }
 }
 
@@ -277,6 +443,21 @@ impl Balance {
     }
 }
 
+/// The current solution for the batch but not yet finale as it can still be replaced by a better
+/// solution.
+#[derive(Clone, Debug, Deserialize, Serialize)]
+enum PendingSolution {
+    /// We observed Trade events but no SolutionSubmission.
+    AccumulatingTrades(Vec<Trade>),
+    /// We observed SolutionSubmission.
+    Submitted {
+        batch_id: BatchId,
+        submitter: UserId,
+        burnt_fees: U256,
+        trades: Vec<Trade>,
+    },
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -287,7 +468,7 @@ mod tests {
     }
 
     fn state_with_fee() -> State {
-        let mut state = State::default();
+        let mut state = State::new();
         let event = TokenListing {
             token: address(0),
             id: 0,
@@ -296,7 +477,7 @@ mod tests {
         state
     }
 
-    fn account_state(state: &State, batch_id: BatchId) -> AccountState {
+    fn account_state(state: &mut State, batch_id: BatchId) -> AccountState {
         AccountState(state.account_state(batch_id).collect())
     }
 
@@ -310,9 +491,9 @@ mod tests {
             batch_id: 0,
         };
         state.apply_event(&Event::Deposit(event), 0).unwrap();
-        let account_state_ = account_state(&state, 0);
+        let account_state_ = account_state(&mut state, 0);
         assert_eq!(account_state_.read_balance(0, address(3)), 0);
-        let account_state_ = account_state(&state, 1);
+        let account_state_ = account_state(&mut state, 1);
         assert_eq!(account_state_.read_balance(0, address(3)), 1);
     }
 
@@ -330,7 +511,7 @@ mod tests {
             state.apply_event(&Event::Deposit(event), 0).unwrap();
         }
 
-        let account_state_ = account_state(&state, 1);
+        let account_state_ = account_state(&mut state, 1);
         assert_eq!(account_state_.read_balance(0, address(3)), 1);
 
         let event = TokenListing {
@@ -339,7 +520,7 @@ mod tests {
         };
         state.apply_event(&Event::TokenListing(event), 0).unwrap();
 
-        let account_state_ = account_state(&state, 1);
+        let account_state_ = account_state(&mut state, 1);
         assert_eq!(account_state_.read_balance(0, address(3)), 1);
         assert_eq!(account_state_.read_balance(1, address(3)), 1);
     }
@@ -356,7 +537,7 @@ mod tests {
             };
             state.apply_event(&Event::Deposit(event), i).unwrap();
 
-            let account_state_ = account_state(&state, i + 2);
+            let account_state_ = account_state(&mut state, i + 2);
             assert_eq!(account_state_.read_balance(0, address(1)), i as u128 + 1);
         }
     }
@@ -373,9 +554,9 @@ mod tests {
             };
             state.apply_event(&Event::Deposit(event), 0).unwrap();
 
-            let account_state_ = account_state(&state, 1);
+            let account_state_ = account_state(&mut state, 1);
             assert_eq!(account_state_.read_balance(0, address(1)), 0);
-            let account_state_ = account_state(&state, 2);
+            let account_state_ = account_state(&mut state, 2);
             assert_eq!(account_state_.read_balance(0, address(1)), i + 1);
         }
     }
@@ -399,7 +580,7 @@ mod tests {
         state
             .apply_event(&Event::WithdrawRequest(event), 0)
             .unwrap();
-        let account_state_ = account_state(&state, 1);
+        let account_state_ = account_state(&mut state, 1);
         assert_eq!(account_state_.read_balance(0, address(1)), 1);
     }
 
@@ -422,7 +603,7 @@ mod tests {
         state
             .apply_event(&Event::WithdrawRequest(event), 1)
             .unwrap();
-        let account_state_ = account_state(&state, 3);
+        let account_state_ = account_state(&mut state, 3);
         assert_eq!(account_state_.read_balance(0, address(1)), 0);
     }
 
@@ -442,7 +623,7 @@ mod tests {
             amount: 1.into(),
         };
         state.apply_event(&Event::Withdraw(event), 1).unwrap();
-        let account_state_ = account_state(&state, 2);
+        let account_state_ = account_state(&mut state, 2);
         assert_eq!(account_state_.read_balance(0, address(1)), 1);
     }
 
@@ -471,7 +652,7 @@ mod tests {
             amount: 2.into(),
         };
         state.apply_event(&Event::Withdraw(event), 1).unwrap();
-        let account_state_ = account_state(&state, 2);
+        let account_state_ = account_state(&mut state, 2);
         // If the pending withdraw was still active the balance would be 0.
         assert_eq!(account_state_.read_balance(0, address(1)), 1);
     }
@@ -518,14 +699,104 @@ mod tests {
         assert_eq!(state.orders(2).next(), None);
         assert_eq!(state.orders(3).next(), None);
 
-        let event = OrderDeletion {
+        let event = Event::OrderDeletion(OrderDeletion {
             owner: address(2),
             id: 0,
-        };
-        state.apply_event(&Event::OrderDeletion(event), 2).unwrap();
+        });
+        assert!(state.apply_event(&event, 2).is_err());
+        state.apply_event(&event, 3).unwrap();
         assert_eq!(state.orders(0).next(), None);
         assert_eq!(state.orders(1).next(), None);
         assert_eq!(state.orders(2).next(), None);
         assert_eq!(state.orders(3).next(), None);
+    }
+
+    #[test]
+    fn trade_and_reversion() {
+        let mut state = state_with_fee();
+        let event = TokenListing {
+            token: address(1),
+            id: 1,
+        };
+        state.apply_event(&Event::TokenListing(event), 0).unwrap();
+
+        for token in 0..2 {
+            for user in 2..4 {
+                let event = Deposit {
+                    user: address(user),
+                    token: address(token),
+                    amount: 10.into(),
+                    batch_id: 0,
+                };
+                state.apply_event(&Event::Deposit(event), 0).unwrap();
+            }
+        }
+
+        let event = OrderPlacement {
+            owner: address(2),
+            index: 0,
+            buy_token: 0,
+            sell_token: 1,
+            valid_from: 0,
+            valid_until: 10,
+            price_numerator: 5,
+            price_denominator: 5,
+        };
+        state.apply_event(&Event::OrderPlacement(event), 0).unwrap();
+        let event = OrderPlacement {
+            owner: address(3),
+            index: 0,
+            buy_token: 1,
+            sell_token: 0,
+            valid_from: 0,
+            valid_until: 10,
+            price_numerator: 3,
+            price_denominator: 3,
+        };
+        state.apply_event(&Event::OrderPlacement(event), 0).unwrap();
+
+        let event = Trade {
+            owner: address(2),
+            order_id: 0,
+            executed_sell_amount: 1,
+            executed_buy_amount: 2,
+            ..Default::default()
+        };
+        state.apply_event(&Event::Trade(event), 1).unwrap();
+        let event = Trade {
+            owner: address(3),
+            order_id: 0,
+            executed_sell_amount: 2,
+            executed_buy_amount: 1,
+            ..Default::default()
+        };
+        state.apply_event(&Event::Trade(event), 1).unwrap();
+        state
+            .apply_event(&Event::SolutionSubmission(SolutionSubmission::default()), 1)
+            .unwrap();
+
+        let account_state_ = account_state(&mut state, 2);
+        assert_eq!(account_state_.read_balance(0, address(2)), 12);
+        assert_eq!(account_state_.read_balance(1, address(2)), 9);
+        assert_eq!(account_state_.read_balance(0, address(3)), 8);
+        assert_eq!(account_state_.read_balance(1, address(3)), 11);
+        assert_eq!(state.orders.get(&(address(2), 0)).unwrap().used_amount, 1);
+        assert_eq!(state.orders.get(&(address(3), 0)).unwrap().used_amount, 2);
+    }
+
+    #[test]
+    fn solution_submission_fee() {
+        let mut state = state_with_fee();
+        let event = SolutionSubmission {
+            submitter: address(1),
+            burnt_fees: 1.into(),
+            ..Default::default()
+        };
+        state
+            .apply_event(&Event::SolutionSubmission(event), 0)
+            .unwrap();
+
+        let account_state_ = account_state(&mut state, 1);
+        assert_eq!(account_state_.read_balance(0, address(1)), 1);
     }
 }
