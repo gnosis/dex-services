0b9a323901d4191be7aed7dbce51d52864a92ab0
josojo <josojo@hotmail.de>
2020-04-08 09:30:27 +0200

    felix comment about removing self.

--- driver/src/price_finding/price_finder_interface.rs
+++ driver/src/price_finding/price_finder_interface.rs
@@ -54,55 +54,55 @@ impl SolverType {
         time_limit: String,
     ) -> Result<Output> {
         match self {
-            SolverType::OpenSolver => self.execute_open_solver(result_folder, input_file),
+            SolverType::OpenSolver => execute_open_solver(result_folder, input_file),
             SolverType::StandardSolver => {
-                self.execute_private_solver(result_folder, input_file, time_limit)
+                execute_private_solver(result_folder, input_file, time_limit)
             }
             SolverType::FallbackSolver => {
-                self.execute_private_solver(result_folder, input_file, time_limit)
+                execute_private_solver(result_folder, input_file, time_limit)
             }
             SolverType::NaiveSolver => {
                 panic!("fn execute should not be called by the naive solver")
             }
         }
     }
-    pub fn execute_open_solver(self, result_folder: &str, input_file: &str) -> Result<Output> {
-        let mut command = Command::new("python");
-        let open_solver_command = command
-            .current_dir("/app/open_solver")
-            .args(&["-m", "src.match"])
-            .arg(format!("{}{}", "/app/", input_file.to_owned()))
-            .arg(format!(
-                "--solution={}{}{}",
-                "/app/",
-                result_folder.to_owned(),
-                "06_solution_int_valid.json",
-            ))
-            .arg(String::from("best-token-pair"));
-        debug!("Using open-solver command `{:?}`", open_solver_command);
-        Ok(open_solver_command.output()?)
-    }
+}
 
-    pub fn execute_private_solver(
-        self,
-        result_folder: &str,
-        input_file: &str,
-        time_limit: String,
-    ) -> Result<Output> {
-        let mut command = Command::new("python");
-        let private_solver_command = command
-            .current_dir("/app/batchauctions")
-            .args(&["-m", "scripts.e2e._run"])
-            .arg(format!("{}{}", "/app/", input_file.to_owned()))
-            .arg(format!("--outputDir={}{}", "/app/", result_folder))
-            .args(&["--solverTimeLimit", &time_limit])
-            .arg(String::from("--useExternalPrices"));
-        debug!(
-            "Using private-solver command `{:?}`",
-            private_solver_command
-        );
-        Ok(private_solver_command.output()?)
-    }
+pub fn execute_open_solver(result_folder: &str, input_file: &str) -> Result<Output> {
+    let mut command = Command::new("python");
+    let open_solver_command = command
+        .current_dir("/app/open_solver")
+        .args(&["-m", "src.match"])
+        .arg(format!("{}{}", "/app/", input_file.to_owned()))
+        .arg(format!(
+            "--solution={}{}{}",
+            "/app/",
+            result_folder.to_owned(),
+            "06_solution_int_valid.json",
+        ))
+        .arg(String::from("best-token-pair"));
+    debug!("Using open-solver command `{:?}`", open_solver_command);
+    Ok(open_solver_command.output()?)
+}
+
+pub fn execute_private_solver(
+    result_folder: &str,
+    input_file: &str,
+    time_limit: String,
+) -> Result<Output> {
+    let mut command = Command::new("python");
+    let private_solver_command = command
+        .current_dir("/app/batchauctions")
+        .args(&["-m", "scripts.e2e._run"])
+        .arg(format!("{}{}", "/app/", input_file.to_owned()))
+        .arg(format!("--outputDir={}{}", "/app/", result_folder))
+        .args(&["--solverTimeLimit", &time_limit])
+        .arg(String::from("--useExternalPrices"));
+    debug!(
+        "Using private-solver command `{:?}`",
+        private_solver_command
+    );
+    Ok(private_solver_command.output()?)
 }
 
 #[cfg_attr(test, automock)]
