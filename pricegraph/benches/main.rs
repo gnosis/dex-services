4edb708bae9635302a154de0cc30ae50438dbb65
Nicholas Rodrigues Lordello <nicholas.lordello@gnosis.pm>
2020-04-09 22:52:44 +0200

    Remove overlapping ring trades

--- pricegraph/benches/main.rs
+++ pricegraph/benches/main.rs
@@ -16,11 +16,11 @@ pub fn orderbook_is_overlapping(c: &mut Criterion) {
     });
 }
 
-pub fn orderbook_update_projection_graph(c: &mut Criterion) {
-    c.bench_function("Orderbook::update_projection_graph", |b| {
+pub fn orderbook_reduce_overlapping_ring_trades(c: &mut Criterion) {
+    c.bench_function("Orderbook::reduce_overlapping_ring_trades", |b| {
         b.iter_batched(
             data::read_default_orderbook,
-            |mut orderbook| orderbook.update_projection_graph(),
+            |mut orderbook| orderbook.reduce_overlapping_ring_trades(),
             BatchSize::SmallInput,
         )
     });
@@ -30,6 +30,6 @@ criterion_group!(
     benches,
     orderbook_read,
     orderbook_is_overlapping,
-    orderbook_update_projection_graph,
+    orderbook_reduce_overlapping_ring_trades,
 );
 criterion_main!(benches);
