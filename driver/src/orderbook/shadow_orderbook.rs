715fd24b3431724125ae11a228926142228fbf32
Valentin Kettner <vakevk+git@gmail.com>
2020-05-04 09:43:54 +0200

    Remove initialization in shadow orderbook

--- driver/src/orderbook/shadow_orderbook.rs
+++ driver/src/orderbook/shadow_orderbook.rs
@@ -59,9 +59,6 @@ impl<'a> StableXOrderBookReading for ShadowedOrderbookReader<'a> {
 
         Ok(orderbook)
     }
-    fn initialize(&self) -> Result<()> {
-        self.primary.initialize()
-    }
 }
 
 /// Background shadow thread that receives orders from the order channel,
@@ -74,9 +71,6 @@ fn background_shadow_reader(
     reader: &dyn StableXOrderBookReading,
     channel: Receiver<(u32, Orderbook)>,
 ) {
-    if let Err(err) = reader.initialize() {
-        log::error!("failed to initialize shadowed orderbook: {:?}", err);
-    }
     while let Ok((batch_id, primary_orderbook)) = channel.recv() {
         let shadow_orderbook = match reader.get_auction_data(batch_id.into()) {
             Ok(orderbook) => orderbook,
