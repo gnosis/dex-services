be3b841f6a73691a942ecf16e1f39db9ddb96676
Felix Leupold <felixleupold90@gmail.com>
2020-06-12 08:58:59 +0200

    Refactor BatchID in its own module (#984)
    * Refactor BatchID in its own module
    
    * refactor BatchId tests

--- core/src/models.rs
+++ core/src/models.rs
@@ -1,9 +1,11 @@
 pub mod account_state;
+pub mod batch_id;
 pub mod order;
 pub mod solution;
 pub mod tokens;
 
 pub use self::account_state::AccountState;
+pub use self::batch_id::BatchId;
 pub use self::order::Order;
 pub use self::solution::ExecutedOrder;
 pub use self::solution::Solution;
