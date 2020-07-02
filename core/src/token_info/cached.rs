302c92cfdf903ce846e0e49aebc414c24ee959db
Felix Leupold <felixleupold90@gmail.com>
2020-07-02 12:47:49 +0200

    Cached TokenInfoFetching

---
+++ core/src/token_info/cached.rs
@@ -0,0 +1,126 @@
+use super::{TokenBaseInfo, TokenId, TokenInfoFetching};
+
+use anyhow::Result;
+use futures::future::{BoxFuture, FutureExt};
+use std::collections::HashMap;
+use std::sync::{Arc, RwLock};
+
+/**
+ * Implementation of TokenInfoFetching that stores previously fetched information in an in-memory cache for fast retrieval.
+ * TokenIds will always be fetched from the inner layer, as new tokens could be added at any time.
+ */
+struct TokenInfoCache {
+    cache: RwLock<HashMap<TokenId, TokenBaseInfo>>,
+    inner: Arc<dyn TokenInfoFetching>,
+}
+
+impl TokenInfoCache {
+    #[allow(dead_code)]
+    pub fn new(inner: Arc<dyn TokenInfoFetching>) -> Self {
+        Self {
+            cache: RwLock::new(HashMap::new()),
+            inner,
+        }
+    }
+}
+
+impl TokenInfoFetching for TokenInfoCache {
+    fn get_token_info<'a>(&'a self, id: TokenId) -> BoxFuture<'a, Result<TokenBaseInfo>> {
+        if let Some(info) = self
+            .cache
+            .read()
+            .ok()
+            .and_then(|cache| cache.get(&id).cloned())
+        {
+            return immediate!(Ok(info));
+        }
+        async move {
+            let info = self.inner.get_token_info(id).await;
+            if let (Ok(info), Ok(mut cache)) = (info.as_ref(), self.cache.write()) {
+                cache.insert(id, info.clone());
+            }
+            info
+        }
+        .boxed()
+    }
+
+    fn all_ids<'a>(&'a self) -> BoxFuture<'a, Result<Vec<TokenId>>> {
+        self.inner.all_ids()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::MockTokenInfoFetching;
+    use super::*;
+    use anyhow::anyhow;
+
+    #[test]
+    fn calls_inner_once_per_token_id_on_success() {
+        let mut inner = MockTokenInfoFetching::new();
+
+        inner.expect_get_token_info().times(1).returning(|_| {
+            immediate!(Ok(TokenBaseInfo {
+                alias: "Foo".to_owned(),
+                decimals: 18,
+            }))
+        });
+
+        let cache = TokenInfoCache::new(Arc::new(inner));
+        let first = cache
+            .get_token_info(1.into())
+            .now_or_never()
+            .expect("First fetch not immediate")
+            .expect("First fetch failed");
+        let second = cache
+            .get_token_info(1.into())
+            .now_or_never()
+            .expect("Second fetch not immediate")
+            .expect("Second fetch failed");
+        assert_eq!(first, second);
+    }
+
+    #[test]
+    fn calls_inner_again_on_error() {
+        let mut inner = MockTokenInfoFetching::new();
+
+        inner
+            .expect_get_token_info()
+            .times(2)
+            .returning(|_| immediate!(Err(anyhow!("error"))));
+
+        let cache = TokenInfoCache::new(Arc::new(inner));
+        cache
+            .get_token_info(1.into())
+            .now_or_never()
+            .expect("First fetch not immediate")
+            .expect_err("Fetch should return error");
+        cache
+            .get_token_info(1.into())
+            .now_or_never()
+            .expect("Second fetch not immediate")
+            .expect_err("Fetch should return error");
+    }
+
+    #[test]
+    fn always_calls_all_ids_on_inner() {
+        let mut inner = MockTokenInfoFetching::new();
+
+        inner
+            .expect_all_ids()
+            .times(2)
+            .returning(|| immediate!(Ok(vec![])));
+
+        let cache = TokenInfoCache::new(Arc::new(inner));
+        cache
+            .all_ids()
+            .now_or_never()
+            .expect("Not Immediate")
+            .expect("First fetch failed");
+        cache
+            .all_ids()
+            .now_or_never()
+            .expect("Not Immediate")
+            .expect("Second fetch failed");
+    }
+}
