9ed4bdf9d81766a305263914d1be898c8082ef1a
Felix Leupold <felixleupold90@gmail.com>
2020-07-02 11:48:16 +0200

    adding an integration test

--- core/src/token_info/onchain.rs
+++ core/src/token_info/onchain.rs
@@ -25,3 +25,54 @@ impl TokenInfoFetching for StableXContractImpl {
         .boxed()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::contracts::web3_provider;
+    use crate::http::HttpFactory;
+    use crate::util::FutureWaitExt;
+    use ethcontract::secret::PrivateKey;
+    use std::time::Duration;
+
+    fn create_contract() -> impl TokenInfoFetching {
+        let http_factory = HttpFactory::default();
+        let web3 = web3_provider(
+            &http_factory,
+            "https://staging-openethereum.mainnet.gnosisdev.com",
+            Duration::from_secs(10),
+        )
+        .expect("Error creating web3");
+        StableXContractImpl::new(
+            &web3,
+            PrivateKey::from_hex_str(
+                "0x0102030405060708091011121314151617181920212223242526272829303132",
+            )
+            .expect("Invalid private key"),
+            1,
+        )
+        .wait()
+        .expect("Error creating contract")
+    }
+
+    #[test]
+    #[ignore]
+    fn integration_test_fetch_mkr() {
+        let contract = create_contract();
+        let info = contract
+            .get_token_info(23.into() /*MKR*/)
+            .wait()
+            .expect("Error fetching token info");
+        assert_eq!(&info.alias, "MKR");
+        assert_eq!(info.decimals, 18);
+    }
+
+    #[test]
+    #[ignore]
+    fn integration_test_all_ids() {
+        let contract = create_contract();
+        let all_ids = contract.all_ids().wait().expect("Error fetching all ids");
+        // On 2020/07/02 68 tokens were listed
+        assert!(all_ids.len() >= 68);
+    }
+}
