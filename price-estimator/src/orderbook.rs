bcee2e917d066cd97de617b0672726aec16e8db0
Nicholas Rodrigues Lordello <nicholas.lordello@gnosis.pm>
2020-07-16 10:44:35 +0200

    implement historic price estimates

--- price-estimator/src/orderbook.rs
+++ price-estimator/src/orderbook.rs
@@ -1,6 +1,6 @@
-use core::orderbook::StableXOrderBookReading;
+use anyhow::Result;
+use core::{models::BatchId, orderbook::StableXOrderBookReading};
 use pricegraph::Pricegraph;
-use std::time::{Duration, SystemTime};
 use tokio::sync::RwLock;
 
 /// Access and update the pricegraph orderbook.
@@ -17,17 +17,11 @@ impl Orderbook {
         }
     }
 
-    pub async fn get_pricegraph(&self) -> Pricegraph {
-        self.pricegraph.read().await.clone()
-    }
-}
-
-impl Orderbook {
-    /// Recreate the pricegraph orderbook.
-    pub async fn update(&self) -> anyhow::Result<()> {
+    /// Creates a `Pricegraph` instance at the given batch ID.
+    async fn create_pricegraph_at_batch(&self, batch_id: BatchId) -> Result<Pricegraph> {
         let (account_state, orders) = self
             .orderbook_reading
-            .get_auction_data(current_batch_id() as u32)
+            .get_auction_data(batch_id.into())
             .await?;
 
         // TODO: Move this cpu heavy computation out of the async function using spawn_blocking.
@@ -37,17 +31,20 @@ impl Orderbook {
                 .map(|order| order.to_element_with_accounts(&account_state)),
         );
 
+        Ok(pricegraph)
+    }
+
+    pub async fn get_pricegraph(&self, batch_id: Option<BatchId>) -> Result<Pricegraph> {
+        match batch_id {
+            Some(batch_id) => self.create_pricegraph_at_batch(batch_id).await,
+            None => Ok(self.pricegraph.read().await.clone()),
+        }
+    }
+
+    /// Recreate the pricegraph orderbook.
+    pub async fn update(&self) -> Result<()> {
+        let pricegraph = self.create_pricegraph_at_batch(BatchId::now()).await?;
         *self.pricegraph.write().await = pricegraph;
         Ok(())
     }
-}
-
-/// Current batch id based on system time.
-fn current_batch_id() -> u64 {
-    const BATCH_DURATION: Duration = Duration::from_secs(300);
-    let now = SystemTime::now();
-    let time_since_epoch = now
-        .duration_since(SystemTime::UNIX_EPOCH)
-        .expect("unix epoch is not in the past");
-    time_since_epoch.as_secs() / BATCH_DURATION.as_secs()
 }
