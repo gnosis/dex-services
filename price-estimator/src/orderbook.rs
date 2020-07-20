f987c8289c712f49ab1dd9d43c93c984e2dde9af
Valentin <vakevk+git@gmail.com>
2020-07-20 08:18:25 +0200

    Store only one Mutex

--- price-estimator/src/orderbook.rs
+++ price-estimator/src/orderbook.rs
@@ -7,7 +7,7 @@ use pricegraph::Pricegraph;
 use tokio::sync::RwLock;
 
 #[derive(Debug)]
-pub enum PricegraphType {
+pub enum PricegraphKind {
     // pricegraph instance with the original orders from the orderbook
     Raw,
     // pricegraph instance with the orders to which the rounding buffer has been applied
@@ -15,31 +15,37 @@ pub enum PricegraphType {
     WithRoundingBuffer,
 }
 
+struct PricegraphCache {
+    pricegraph_raw: Pricegraph,
+    pricegraph_with_rounding_buffer: Pricegraph,
+}
+
 /// Access and update the pricegraph orderbook.
 pub struct Orderbook {
     orderbook_reading: Box<dyn StableXOrderBookReading>,
-    pricegraph_raw: RwLock<Pricegraph>,
-    pricegraph_with_rounding_buffer: RwLock<Pricegraph>,
+    pricegraph_cache: RwLock<PricegraphCache>,
 }
 
 impl Orderbook {
     pub fn new(orderbook_reading: Box<dyn StableXOrderBookReading>) -> Self {
         Self {
             orderbook_reading,
-            pricegraph_raw: RwLock::new(Pricegraph::new(std::iter::empty())),
-            pricegraph_with_rounding_buffer: RwLock::new(Pricegraph::new(std::iter::empty())),
+            pricegraph_cache: RwLock::new(PricegraphCache {
+                pricegraph_raw: Pricegraph::new(std::iter::empty()),
+                pricegraph_with_rounding_buffer: Pricegraph::new(std::iter::empty()),
+            }),
         }
     }
 
     pub async fn pricegraph(
         &self,
         batch_id: Option<BatchId>,
-        pricegraph_type: PricegraphType,
+        pricegraph_type: PricegraphKind,
     ) -> Result<Pricegraph> {
         match batch_id {
             Some(batch_id) => {
                 let mut auction_data = self.auction_data(batch_id).await?;
-                if matches!(pricegraph_type, PricegraphType::WithRoundingBuffer) {
+                if matches!(pricegraph_type, PricegraphKind::WithRoundingBuffer) {
                     self.apply_rounding_buffer_to_auction_data(&mut auction_data)
                         .await;
                 }
@@ -55,12 +61,18 @@ impl Orderbook {
 
         // TODO: Move this cpu heavy computation out of the async function using spawn_blocking.
         let pricegraph = pricegraph_from_auction_data(&auction_data);
-        *self.pricegraph_raw.write().await = pricegraph;
+        self.pricegraph_cache
+            .write()
+            .await
+            .pricegraph_with_rounding_buffer = pricegraph;
 
         self.apply_rounding_buffer_to_auction_data(&mut auction_data)
             .await;
         let pricegraph = pricegraph_from_auction_data(&auction_data);
-        *self.pricegraph_with_rounding_buffer.write().await = pricegraph;
+        self.pricegraph_cache
+            .write()
+            .await
+            .pricegraph_with_rounding_buffer = pricegraph;
 
         Ok(())
     }
@@ -75,13 +87,12 @@ impl Orderbook {
         // TODO
     }
 
-    async fn cached_pricegraph(&self, pricegraph_type: PricegraphType) -> Pricegraph {
+    async fn cached_pricegraph(&self, pricegraph_type: PricegraphKind) -> Pricegraph {
+        let cache = self.pricegraph_cache.read().await;
         match pricegraph_type {
-            PricegraphType::Raw => &self.pricegraph_raw,
-            PricegraphType::WithRoundingBuffer => &self.pricegraph_with_rounding_buffer,
+            PricegraphKind::Raw => &cache.pricegraph_raw,
+            PricegraphKind::WithRoundingBuffer => &cache.pricegraph_with_rounding_buffer,
         }
-        .read()
-        .await
         .clone()
     }
 }
