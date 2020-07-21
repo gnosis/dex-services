44bc11ab6351575fdb71f9fdf532001238757d17
Valentin <vakevk+git@gmail.com>
2020-07-21 12:37:39 +0200

    Introduce mockable trait for async sleeping
    This is useful in tests and will be used in another PR soon.

--- core/src/util.rs
+++ core/src/util.rs
@@ -1,5 +1,6 @@
 use ethcontract::U256;
-use std::future::Future;
+use futures::future::{BoxFuture, FutureExt as _};
+use std::{future::Future, time::Duration};
 
 pub trait CeiledDiv {
     /// Panics on overflow.
@@ -56,6 +57,15 @@ where
         futures::executor::block_on(self)
     }
 }
+
+#[cfg_attr(test, mockall::automock)]
+pub trait AsyncSleeping: Send + Sync {
+    fn sleep(&self, duration: Duration) -> BoxFuture<'static, ()> {
+        async_std::task::sleep(duration).boxed()
+    }
+}
+pub struct AsyncSleep;
+impl AsyncSleeping for AsyncSleep {}
 
 #[cfg(test)]
 pub mod test_util {
