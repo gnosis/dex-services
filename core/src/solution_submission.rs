297cdeb8d1fa87cc667ec0e19c7450136d8e597e
Valentin <vakevk+git@gmail.com>
2020-07-27 13:19:33 +0200

    Silence "nonce already used error" temporarily (#1195)
    This is a race condition that will be fixed in two future PRs: #1143.
    Until those PRs had time to be tested in staging we do not want to be notified
    of these errors.
    The code here is copied from the `is_nonce_error` function in that PR.
    
    Part of #1192.
    
    ### Test Plan
    Didn't add unit test for this because it would just parrot back the function and the code is going to be removed soon anyway.

--- core/src/solution_submission.rs
+++ core/src/solution_submission.rs
@@ -8,9 +8,12 @@ use crate::{
 
 use anyhow::{anyhow, Error, Result};
 use async_std::future::TimeoutError;
-use ethcontract::errors::{ExecutionError, MethodError};
-use ethcontract::web3::types::TransactionReceipt;
-use ethcontract::U256;
+use ethcontract::{
+    errors::{ExecutionError, MethodError},
+    jsonrpc::types::Error as RpcError,
+    web3::{error::Error as Web3Error, types::TransactionReceipt},
+    U256,
+};
 use futures::future::{BoxFuture, FutureExt as _};
 use log::info;
 use pricegraph::num;
@@ -132,6 +135,21 @@ impl<'a> StableXSolutionSubmitter<'a> {
         solution: Solution,
         err: MethodError,
     ) -> SolutionSubmissionError {
+        // There is a race condition that will be fixed in a future PR that results in this
+        // error when there was actually no problem. In that PR we will remove this code.
+        match &err.inner {
+            ExecutionError::Web3(Web3Error::Rpc(RpcError { code, message, .. }))
+                if code.code() == -32010
+                    && message.find("Transaction nonce is too low.").is_some() =>
+            {
+                return SolutionSubmissionError::Benign(format!(
+                    "Likely benign nonce error: {:?}",
+                    err
+                ));
+            }
+            _ => (),
+        }
+
         if let Some(tx) = extract_transaction_receipt(&err) {
             if let Some(block_number) = tx.block_number {
                 if let Err(err) = self
