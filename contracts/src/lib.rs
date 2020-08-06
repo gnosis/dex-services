55fb2ebbf3db0e23f3abc03c83a1c27207077116
Nicholas Rodrigues Lordello <nlordell@gmail.com>
2020-08-06 10:08:11 +0200

    Implement contract deployment in Rust (#689)
    This PR adds a new `contract` crate with a `deploy` script to deploy exchange contracts to a development network.
    
    This an initial step towards #846 where we need an alternative to `scripts/setup_contracts.sh` which uses Truffle. The next steps in order to no longer require NodeJS are:
    - Update `core` and `e2e` to use the new `contracts` crate.
    - Update the Travis build file to use `cargo run -p contracts --bin deploy` instead of performing a Truffle migration.
    - Use the `@gnosis.pm/dex-contracts` npm package for retrieving the contract artifact instead of building `dex-contracts` with Truffle.
    - Update the README with new build instructions.
    
    ### Test Plan
    
    Run the deploy script:
    ```
    $ (cd contracts; cargo run -p contracts --bin deploy --features bin)
       Compiling contracts v0.1.0 (/var/home/nlordell/Developer/dex-services/contracts)
        Finished dev [unoptimized + debuginfo] target(s) in 12.14s
         Running `/var/home/nlordell/Developer/dex-services/target/debug/deploy`
    [2020-08-05T12:43:07Z INFO  deploy] checking connection to local test node http://localhost:8545
    [2020-08-05T12:43:07Z INFO  deploy] deploying library contracts
    [2020-08-05T12:43:07Z INFO  deploy] deployed IdToAddressBiMap to 0x65dbc7c034b644401ad2a1f1ed8b284ae41e56bc
    [2020-08-05T12:43:07Z INFO  deploy] deployed IterableAppendOnlySet to 0xc27e5a197f7f1c794bcd348f673d80687eff83a6
    [2020-08-05T12:43:07Z INFO  deploy] deploying fee token contracts
    [2020-08-05T12:43:07Z INFO  deploy] deployed TokenOWL to 0x8f534a8084d9306527ff15c919f7fa691c0ca856
    [2020-08-05T12:43:07Z INFO  deploy] deployed TokenOWLProxy to 0x445d64d63f42401c8c7ff846d2a92edd2b31417b
    [2020-08-05T12:43:07Z INFO  deploy] deploying exchange and viewer contracts
    [2020-08-05T12:43:07Z INFO  deploy] deployed BatchExchange to 0x5355d6d088ddc1a99b29114976a6ca0321d71fa6
    [2020-08-05T12:43:07Z INFO  deploy] deployed BatchExchangeViewer to 0x63f7170efe984d40d1a435a917b99c71ad645901
    ```
    
    Then check that the `BatchExchange` contract address matches in the generated code:
    ```
    $ for byte in $(echo '5355d6d088ddc1a99b29114976a6ca0321d71fa6' | grep -o ..); do printf "%d\n" "0x$byte"; done | xargs
    83 85 214 208 136 221 193 169 155 41 17 73 118 166 202 3 33 215 31 166
    $ cargo build && cat target/debug/build/contracts-*/out/BatchExchange.rs | grep -C 3 Address::from
       Compiling contracts v0.1.0 (/var/home/nlordell/Developer/dex-services/contracts)
        Finished dev [unoptimized + debuginfo] target(s) in 4.01s
                let artifact = Self::artifact();
                let network = match network_id {
                    "5777" => self::ethcontract::common::truffle::Network {
                        address: self::ethcontract::Address::from([
                            83, 85, 214, 208, 136, 221, 193, 169, 155, 41, 17, 73, 118, 166, 202, 3,
                            33, 215, 31, 166,
                        ]),
    ```

---
+++ contracts/src/lib.rs
@@ -0,0 +1,13 @@
+// TODO(nlordell): Remove this lint once we release a new `ethcontract` version
+// that does not trigger this lint.
+#![allow(unused_braces)]
+
+#[cfg(feature = "bin")]
+pub mod paths;
+
+include!(concat!(env!("OUT_DIR"), "/BatchExchange.rs"));
+include!(concat!(env!("OUT_DIR"), "/BatchExchangeViewer.rs"));
+include!(concat!(env!("OUT_DIR"), "/IdToAddressBiMap.rs"));
+include!(concat!(env!("OUT_DIR"), "/IterableAppendOnlySet.rs"));
+include!(concat!(env!("OUT_DIR"), "/TokenOWL.rs"));
+include!(concat!(env!("OUT_DIR"), "/TokenOWLProxy.rs"));
