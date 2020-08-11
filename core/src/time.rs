3f675390f7d6a629c58e0579188a207b84948b85
Nicholas Rodrigues Lordello <nlordell@gmail.com>
2020-08-11 12:55:03 +0200

    Add SystemTime utilities

---
+++ core/src/time.rs
@@ -0,0 +1,24 @@
+//! Time utilites for dealing with batches.
+
+use std::time::{Duration, SystemTime, SystemTimeError};
+
+/// `SystemTime` extention trait.
+pub trait SystemTimeExt {
+    /// Creates a new `SystemTime` from a Unix timestamp.
+    ///
+    /// Returns `None` if the specified timestamp cannot be represented.
+    fn from_timestamp(timestamp: u64) -> Option<SystemTime> {
+        SystemTime::UNIX_EPOCH.checked_add(Duration::from_secs(timestamp))
+    }
+
+    /// Convert the system time into a timestamp in seconds from the Unix epoch.
+    ///
+    /// Returns an error if the system time is earlier the Unix epoch.
+    fn as_timestamp(&self) -> Result<u64, SystemTimeError>;
+}
+
+impl SystemTimeExt for SystemTime {
+    fn as_timestamp(&self) -> Result<u64, SystemTimeError> {
+        Ok(self.duration_since(SystemTime::UNIX_EPOCH)?.as_secs())
+    }
+}
