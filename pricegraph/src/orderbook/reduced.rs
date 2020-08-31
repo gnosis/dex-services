91fbfa3a2b7cca88ed0a08cb62338c39ec4471ba
Nicholas Rodrigues Lordello <nlordell@gmail.com>
2020-08-31 21:00:27 +0200

    use new transitive order iterator

--- pricegraph/src/orderbook/reduced.rs
+++ pricegraph/src/orderbook/reduced.rs
@@ -20,39 +20,12 @@ impl ReducedOrderbook {
         TransitiveOrders::new(self.0, pair).expect("negative cycle in reduced orderbook")
     }
 
-    /// Fills the optimal transitive order for the specified token pair. This
-    /// method is similar to
-    /// `ReducedOrderbook::fill_optimal_transitive_order_if` except it does not
-    /// check a condition on the discovered path's flow before filling.
-    pub fn fill_optimal_transitive_order(&mut self, pair: TokenPair) -> Option<Flow> {
-        self.fill_optimal_transitive_order_if(pair, |_| true)
-    }
-
     /// Finds and returns the optimal transitive order for the specified token
     /// pair without filling it. Returns `None` if no such transitive order
     /// exists.
     pub fn find_optimal_transitive_order(&mut self, pair: TokenPair) -> Option<Flow> {
         self.0
             .find_optimal_transitive_order(pair)
-            .expect("negative cycle in reduced orderbook")
-    }
-
-    /// Fills the optimal transitive order (i.e. with the lowest exchange rate)
-    /// for the specified token pair by pushing flow from the buy token to the
-    /// sell token, if the condition is met. The trading path through the
-    /// orderbook graph is filled to maximum capacity, reducing the remaining
-    /// order amounts and user balances along the way, returning the flow for
-    /// the path.
-    ///
-    /// Returns `None` if the condition is not met or there is no path between
-    /// the token pair.
-    pub fn fill_optimal_transitive_order_if(
-        &mut self,
-        pair: TokenPair,
-        condition: impl FnMut(&Flow) -> bool,
-    ) -> Option<Flow> {
-        self.0
-            .fill_optimal_transitive_order_if(pair, condition)
             .expect("negative cycle in reduced orderbook")
     }
 
