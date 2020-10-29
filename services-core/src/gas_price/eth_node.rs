1984c8dfe881f6b3c0ccfdceabda716beca3d5b8
Valentin <vakevk+git@gmail.com>
2020-10-29 11:08:03 +0100

    Add GasNow gas price estimator (#1496)
    They predict based on the pending transaction queue and the currently
    mining block (in sparkpool).
    This estimation should be more accurate than the gnosis gas station and
    react more quickly to changes in gas price.
    
    This is part of an effort to have better gas price estimates (also for oba). I will also make a similar implementation based on pending blocks reported by our ethereum node.
    
    Btw this service also offers sending transactions directly to their mining pool. This allows for potentially faster transactions because the transactions and also potentially private transactions where they ensure that only they mine the transaction and do not broadcast.
    Just noting that, not saying we should use it.
    
    ### Test Plan
    New tests.

--- services-core/src/gas_price/eth_node.rs
+++ services-core/src/gas_price/eth_node.rs
@@ -8,11 +8,7 @@ use std::time::Duration;
 
 #[async_trait::async_trait]
 impl GasPriceEstimating for Web3 {
-    async fn estimate(&self) -> Result<U256> {
-        self.eth().gas_price().await.map_err(From::from)
-    }
-
     async fn estimate_with_limits(&self, _gas_limit: U256, _time_limit: Duration) -> Result<U256> {
-        self.estimate().await
+        self.eth().gas_price().await.map_err(From::from)
     }
 }
